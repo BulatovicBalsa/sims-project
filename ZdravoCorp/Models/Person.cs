c397ceeeacff19b95173afe70a4cfc2c8f500c4f
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-04-03 17:33:14 +0200

    project initialization

---
+++ ZdravoCorp/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZdravoCorp.Models
+{
+    public class Person
+    {
+        static int freeId = 0;
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JMBG { get; set; }
+        public Person(string firstName, string lastName, string jmbg)
+        {
+            Id = freeId;
+            freeId++;
+            FirstName = firstName;
+            LastName = lastName;
+            JMBG = jmbg;
+        }
+    }
+}
---
+++ ZdravoCorp/Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZdravoCorp.Models
+{
+    public class Profile
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+        public Profile(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
