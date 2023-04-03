d41c066e0dc4848a44840e533a12c9fbb2acd85f
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-04-04 01:02:42 +0200

    added Services folder

---
+++ ZdravoCorp/Services/IService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZdravoCorp.Services
+{
+    public interface IService<T>
+    {
+        IEnumerable<T> GetAll();
+        T? GetById(int id);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
