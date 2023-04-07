d909e432aec69680fa01a359ead05c5d6091ef3f
Vladimir Popov <vladapopov02@gmail.com>
2023-04-07 23:49:13 +0200

    feat: Add Patient model

---
+++ Hospital/Models/Patient/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Models.Patient
+{
+    public class Patient : Person
+    {
+        public MedicalRecord MedicalRecord { get; set; }
+
+        public Patient(string firstName, string lastName, string jmbg, string username, string password, MedicalRecord medicalRecord) : base(firstName, lastName, jmbg, username, password)
+        {
+            MedicalRecord = medicalRecord;
+        }
+    }
+}
