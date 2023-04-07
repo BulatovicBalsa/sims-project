5e387739892a447ce286edaed2e457d69bde119f
MomirMilutinovic <milutinm462@gmail.com>
2023-04-07 06:08:57 -0700

    feat: Adds EquipmentRepository

---
+++ Hospital/Repositories/Manager/EquipmentRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Models.Manager;
+using CsvHelper;
+
+namespace Hospital.Repositories.Manager
+{
+
+
+    internal class EquipmentRepository
+    {
+        private const String FilePath = "../../equipment.csv";
+
+        public List<Equipment> GetAll()
+        {
+            using StreamReader reader = new StreamReader(FilePath);
+            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+            return csvReader.GetRecords<Equipment>().ToList();
+        }
+
+        public Equipment? GetById(int id)
+        {
+            return GetAll().First(equipment => equipment.Id == id);
+        }
+        
+        public void Update(Equipment equipment)
+        {
+            var allEquipment = GetAll();
+
+            var indexToUpdate = allEquipment.FindIndex(e => e.Id == equipment.Id);            
+            if (indexToUpdate == -1)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            allEquipment[indexToUpdate] = equipment;
+
+            using StreamWriter writer = new StreamWriter(FilePath);
+            using CsvWriter csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            csvWriter.WriteRecords(allEquipment);
+        }
+
+        public void Delete(Equipment equipment)
+        {
+            var allEquipment = GetAll();
+
+            var indexToDelete = allEquipment.FindIndex(e => e.Id == equipment.Id);
+            if (indexToDelete == -1)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            allEquipment.RemoveAt(indexToDelete);
+
+            //TODO: Extract saving to file
+            using StreamWriter writer = new StreamWriter(FilePath);
+            using CsvWriter csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            csvWriter.WriteRecords(allEquipment);
+        }
+    }
+}
