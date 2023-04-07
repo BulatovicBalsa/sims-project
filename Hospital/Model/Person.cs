6e3bccff403db29401084d5191f36cbe63e3e041
Vladimir Popov <vladapopov02@gmail.com>
2023-04-07 22:52:55 +0200

    feat: Added Person model

---
+++ Hospital/Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Model
+{
+    public class Person
+    {
+        public int Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Jmbg { get; set; }
+        public Profile Profile { get; set; }
+
+        public Person(string firstName, string lastName, string jmbg, string username, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Jmbg = jmbg;
+            Profile = new Profile(username, password);
+        }
+    }
+}
