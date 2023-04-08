4ab332bfc02b02f2f446adfd62d3eae55df16c44
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-04-08 22:04:34 +0200

    feat: Add Doctor model

---
+++ Hospital/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace Hospital.Models
+{
+    public class Person
+    {
+        static int freeId = 0;
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JMBG { get; set; }
+        public Profile Profile { get; set; }
+        public Person(string firstName, string lastName, string jmbg, string username, string password)
+        {
+            Id = freeId;
+            freeId++;
+            FirstName = firstName;
+            LastName = lastName;
+            JMBG = jmbg;
+            Profile = new Profile(username, password);
+        }
+
+    }
+}
