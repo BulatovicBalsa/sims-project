df9547ff2a80f81120fa66c1b2525253dd12d4ed
MomirMilutinovic <milutinm462@gmail.com>
2023-04-09 04:30:59 -0700

    fix: Serializer returns empty list if the file it reads is not found

--- Hospital/Serialization/Serializer.cs
+++ Hospital/Serialization/Serializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -10,9 +11,17 @@ public class Serializer<T>
 {
     public static List<T> FromCSV(string filePath)
     {
-        using var reader = new StreamReader(filePath);
-        using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
-        return csvReader.GetRecords<T>().ToList();
+        try
+        {
+            using var reader = new StreamReader(filePath);
+            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+            return csvReader.GetRecords<T>().ToList();
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine(e);
+            return new List<T>();
+        }
     }
 
     public static void ToCSV(List<T> records, string filePath)
