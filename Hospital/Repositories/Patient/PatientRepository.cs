cf354bf4d655e76daf2f9a2acb8fc33a79bca004
Vladimir Popov <vladapopov02@gmail.com>
2023-04-09 21:13:49 +0200

    feat: Add MedicalRecord CSV mappers (for read and write separately)

--- Hospital/Repositories/Patient/PatientRepository.cs
+++ Hospital/Repositories/Patient/PatientRepository.cs
@@ -12,13 +12,51 @@ using Hospital.Serialization;
 namespace Hospital.Repositories.Patient
 {
     using Hospital.Models.Patient;
+
+    public sealed class PatientWriteMapper : ClassMap<Patient>
+    {
+        public PatientWriteMapper()
+        {
+            Map(patient => patient.Id).Index(0);
+            Map(patient => patient.FirstName).Index(1);
+            Map(patient => patient.LastName).Index(2);
+            Map(patient => patient.Jmbg).Index(3);
+            Map(patient => patient.Profile.Username).Index(4);
+            Map(patient => patient.Profile.Password).Index(5);
+            Map(patient => patient.MedicalRecord.Height).Index(6);
+            Map(patient => patient.MedicalRecord.Weight).Index(7);
+            Map(patient => patient.MedicalRecord.Allergies).Convert(row => string.Join("|", row.Value.MedicalRecord.Allergies)).Index(8);
+            Map(patient => patient.MedicalRecord.MedicalHistory).Convert(row => string.Join("|", row.Value.MedicalRecord.MedicalHistory)).Index(9);
+        }
+    }
+
+    public sealed class PatientReadMapper : ClassMap<Patient>
+    {
+        private List<string> SplitColumnValues(string? columnValue)
+        {
+            return columnValue?.Split("|").ToList() ?? new List<string>();
+        }
+        public PatientReadMapper()
+        {
+            Map(patient => patient.Id).Index(0);
+            Map(patient => patient.FirstName).Index(1);
+            Map(patient => patient.LastName).Index(2);
+            Map(patient => patient.Jmbg).Index(3);
+            Map(patient => patient.Profile.Username).Index(4);
+            Map(patient => patient.Profile.Password).Index(5);
+            Map(patient => patient.MedicalRecord.Height).Index(6);
+            Map(patient => patient.MedicalRecord.Weight).Index(7);
+            Map(patient => patient.MedicalRecord.Allergies).Index(8).Convert(row => SplitColumnValues(row.Row.GetField<string>("Allergies")));
+            Map(patient => patient.MedicalRecord.MedicalHistory).Index(9).Convert(row => SplitColumnValues(row.Row.GetField<string>("MedicalHistory")));
+        }
+    }
     public class PatientRepository
     {
         private const string FilePath = "..\\..\\..\\Data\\patients.csv";
 
         public List<Patient> GetAll()
         {
-            return Serializer<Patient>.FromCSV(FilePath);
+            return Serializer<Patient>.FromCSV(FilePath, new PatientReadMapper());
         }
 
         public Patient? GetById(string id)
@@ -30,7 +68,7 @@ namespace Hospital.Repositories.Patient
         {
             var allPatients = GetAll();
             allPatients.Add(patient);
-            Serializer<Patient>.ToCSV(allPatients, FilePath);
+            Serializer<Patient>.ToCSV(allPatients, FilePath, new PatientWriteMapper());
         }
 
         public void Update(Patient patient)
@@ -42,7 +80,7 @@ namespace Hospital.Repositories.Patient
                 throw new KeyNotFoundException($"Patient with id {patient.Id} was not found.");
             allPatients[indexToUpdate] = patient;
 
-            Serializer<Patient>.ToCSV(allPatients, FilePath);
+            Serializer<Patient>.ToCSV(allPatients, FilePath, new PatientWriteMapper());
         }
 
         public void Delete(Patient patient)
@@ -52,7 +90,7 @@ namespace Hospital.Repositories.Patient
             if (!patientRecords.Remove(patient))
                 throw new KeyNotFoundException($"Patient with id {patient.Id} was not found.");
 
-            Serializer<Patient>.ToCSV(patientRecords, FilePath);
+            Serializer<Patient>.ToCSV(patientRecords, FilePath, new PatientWriteMapper());
         }
     }
 }
