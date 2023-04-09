5bd0cf252cf5bf4914b2660de74046c25ad89467
Vladimir Popov <vladapopov02@gmail.com>
2023-04-09 18:37:40 +0200

    feat: Add empty construstor in Profile class

--- Hospital/Models/Profile.cs
+++ Hospital/Models/Profile.cs
@@ -5,6 +5,11 @@
         public string Username { get; set; }
         public string Password { get; set; }
 
+        public Profile()
+        {
+            Username = "";
+            Password = "";
+        }
         public Profile(string username, string password)
         {
             Username = username;
