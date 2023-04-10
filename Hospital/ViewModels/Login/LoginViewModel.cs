f056005cbd69da7137f1d39b8713215acbecea66
Vladimir Popov <vladapopov02@gmail.com>
2023-04-10 21:52:51 +0200

    feat: Add LoginViewModel delegates and properties

---
+++ Hospital/ViewModels/Login/LoginViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Hospital.ViewModels.Login
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public SecureString Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage {
+            get => _errorMessage;
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible {
+            get => _isViewVisible;
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand ShowPassowrdCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            var emptyOrShortFields = string.IsNullOrWhiteSpace(Username) || Username.Length < 4 ||
+                                      Password.Length < 4 || string.IsNullOrWhiteSpace(Password.ToString());
+            
+            return !emptyOrShortFields;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
