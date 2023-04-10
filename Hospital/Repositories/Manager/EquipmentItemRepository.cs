1e86b7a096b895b87428043333567c9af163fbef
MomirMilutinovic <milutinm462@gmail.com>
2023-04-10 11:38:21 -0700

    feat: Store EquipmentItem objects in a file

---
+++ Hospital/Repositories/Manager/EquipmentItemRepository.cs
@@ -0,0 +1,56 @@
+﻿using Hospital.Models.Manager;
+using Hospital.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories.Manager
+{
+    public class EquipmentItemRepository
+    {
+        private const string FilePath = "../../../Data/equipmentItems.csv";
+
+        public List<EquipmentItem> GetAll()
+        {
+            return Serializer<EquipmentItem>.FromCSV(FilePath);
+        }
+
+        public void Add(EquipmentItem equipmentItem)
+        {
+            var equipmentItems = GetAll();
+            
+            equipmentItems.Add(equipmentItem);
+
+            Serializer<EquipmentItem>.ToCSV(equipmentItems, FilePath);
+
+        }
+
+        public void Update(EquipmentItem equipmentItem)
+        {
+            var equipmentItems = GetAll();
+
+            var indexToUpdate = equipmentItems.FindIndex(e => e.EquipmentId == equipmentItem.EquipmentId && e.RoomId == equipmentItem.RoomId);
+            if (indexToUpdate == -1) throw new KeyNotFoundException();
+
+            equipmentItems[indexToUpdate] = equipmentItem;
+
+            Serializer<EquipmentItem>.ToCSV(equipmentItems, FilePath);
+        }
+
+        public void Delete(EquipmentItem equipmentItem)
+        {
+            var equipmentItems = GetAll();
+
+            var indexToDelete = equipmentItems.FindIndex(e => e.EquipmentId == equipmentItem.EquipmentId && e.RoomId == equipmentItem.RoomId);
+            if (indexToDelete == -1) throw new KeyNotFoundException();
+
+            equipmentItems.RemoveAt(indexToDelete);
+
+            Serializer<EquipmentItem>.ToCSV(equipmentItems, FilePath);
+            
+        }
+
+    }
+}
---
+++ HospitalTests/Repositories/Manager/EquipmentItemRepositoryTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hospital.Repositories.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Models.Manager;
+using Hospital.Serialization;
+
+namespace HospitalTests.Repositories.Manager
+{
+    [TestClass()]
+    public class EquipmentItemRepositoryTests
+    {
+        [TestMethod()]
+        public void TestGetAll()
+        {
+            var equipmentItems = new List<EquipmentItem>()
+            {
+                new("1", "1", 1),
+                new("2", "2", 2)
+            };
+            Serializer<EquipmentItem>.ToCSV(equipmentItems, "../../../Data/equipmentItems.csv");
+
+            Assert.AreEqual(2, new EquipmentItemRepository().GetAll().Count);
+        }
+
+        [TestMethod()]
+        public void TestAdd()
+        {
+            var equipmentItems = new List<EquipmentItem>()
+            {
+                new("1", "1", 1),
+                new("2", "2", 2)
+            };
+            Serializer<EquipmentItem>.ToCSV(equipmentItems, "../../../Data/equipmentItems.csv");
+
+            var equipmentItemRepository = new EquipmentItemRepository();
+            equipmentItemRepository.Add(new EquipmentItem("3", "3", 3));
+
+            Assert.AreEqual(3, equipmentItemRepository.GetAll().Count);
+        }
+
+        [TestMethod()]
+        public void TestUpdate()
+        {
+            var equipmentItems = new List<EquipmentItem>()
+            {
+                new("2", "1", 1),
+                new("2", "2", 2)
+            };
+            Serializer<EquipmentItem>.ToCSV(equipmentItems, "../../../Data/equipmentItems.csv");
+
+            var equipmentItemRepository = new EquipmentItemRepository();
+            equipmentItemRepository.Update(new EquipmentItem("2", "2", 3));
+
+            Assert.AreEqual(3, equipmentItemRepository.GetAll()[1].Amount);
+        }
+
+        [TestMethod()]
+        public void TestDelete()
+        {
+            var equipmentItems = new List<EquipmentItem>()
+            {
+                new("2", "1", 1),
+                new("2", "2", 2)
+            };
+            Serializer<EquipmentItem>.ToCSV(equipmentItems, "../../../Data/equipmentItems.csv");
+
+            var equipmentItemRepository = new EquipmentItemRepository();
+            equipmentItemRepository.Delete(equipmentItems[1]);
+
+            Assert.AreEqual(1, equipmentItemRepository.GetAll().Count);
+            Assert.AreEqual(1, equipmentItemRepository.GetAll()[0].Amount);
+            Assert.AreEqual("1", equipmentItemRepository.GetAll()[0].RoomId);
+         }
+    }
+}
