9ab64d3d8afc2a04b491e67ed0ee162f51778f0e
Vladimir Popov <vladapopov02@gmail.com>
2023-04-10 23:40:05 +0200

    feat: Add login service to check for user credentials

---
+++ Hospital/Services/LoginService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Models;
+using Hospital.Repositories.Doctor;
+using Hospital.Repositories.Nurse;
+using Hospital.Repositories.Patient;
+
+namespace Hospital.Services
+{
+    public class LoginService
+    {
+        private DoctorRepository _doctorRepository;
+        private NurseRepository _nurseRepository;
+        private PatientRepository _patientRepository;
+
+        public Person? LoggedUser { get; set; }
+
+        public LoginService()
+        {
+            _doctorRepository = new DoctorRepository();
+            _nurseRepository = new NurseRepository();
+            _patientRepository = new PatientRepository();
+        }
+
+        public bool AuthenticateUser(NetworkCredential credential)
+        {
+            LoggedUser = _doctorRepository.GetByUsername(credential.UserName);
+            if(LoggedUser != null && LoggedUser.Profile.Password == credential.Password)
+            {
+                return true;
+            }
+
+            LoggedUser = _doctorRepository.GetByUsername(credential.UserName);
+            if (LoggedUser != null && LoggedUser.Profile.Password == credential.Password)
+            {
+                return true;
+            }
+
+            LoggedUser = _patientRepository.GetByUsername(credential.UserName);
+            if (LoggedUser != null && LoggedUser.Profile.Password == credential.Password)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
