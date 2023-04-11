a61ab20335e926a68b68d3bfdc1359754401cf47
Teodor Vidakovic <teodorv22@gmail.com>
2023-04-11 19:32:53 +0200

    feat: added patientModelView but still not done

---
+++ Hospital/ViewModels/PatientViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.CompilerServices;
+using Hospital.Models.Examination;
+using Hospital.Models.Patient;
+using Hospital.Repositories.Examinaton;
+
+
+namespace Hospital.ViewModels
+{
+
+    public class PatientViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Examination> _examinations;
+        private readonly ExaminationRepository _examinationRepository;
+
+        public ObservableCollection<Examination> Examinations
+        {
+            get { return _examinations;}
+            set
+            {
+                _examinations = value;
+                OnPropertyChanged(nameof(Examinations));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public PatientViewModel(ExaminationRepository examinationRepository)
+        {
+            _examinationRepository = examinationRepository;
+        }
+
+        public void LoadExaminations(Patient patient)
+        {
+            var examinations = _examinationRepository.GetAll(patient);
+            Examinations = new ObservableCollection<Examination>(examinations);
+        }
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
--- Hospital/Views/PatientView.xaml.cs
+++ Hospital/Views/PatientView.xaml.cs
@@ -7,14 +7,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using Hospital.Models.Patient;
+using Hospital.Repositories.Examinaton;
+using Hospital.Repositories.Patient;
+using Hospital.ViewModels;
 
 namespace Hospital.Views
 {
     public partial class PatientView : Window
     {
-        public PatientView()
+        private PatientViewModel _viewModel;
+        private ExaminationRepository _examinationRepository;
+
+        public PatientView(Patient patient)
         {
             InitializeComponent();
+
+            _examinationRepository =
+                new ExaminationRepository(new ExaminationChangesTracker(new ExaminationChangesTrackerRepository()));
+            _viewModel = new PatientViewModel(_examinationRepository);
+            _viewModel.LoadExaminations(patient); 
+            
+            this.DataContext = _viewModel;
+
         }
 
         private void PatientView_OnMouseDown(object sender, MouseButtonEventArgs e)
@@ -36,6 +51,16 @@ namespace Hospital.Views
         }
 
         private void BtnAddExamination_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnUpdateExamination_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteExamination_Click(object sender, RoutedEventArgs e)
         {
 
         }
