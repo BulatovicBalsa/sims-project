5e08ba4da82dd9350423656786babb511e2cb18e
Teodor Vidakovic <teodorv22@gmail.com>
2023-04-11 17:50:08 +0200

    feat: added table header for examinations

---
+++ Hospital/Views/PatientView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Hospital.Views
+{
+    public partial class PatientView : Window
+    {
+        public PatientView()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginView_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void BtnMinimize_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_OnClick_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+    }
+}
