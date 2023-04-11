8195f849b35e2c27e79b828009f41d8baf78347e
Vladimir Popov <vladapopov02@gmail.com>
2023-04-11 19:25:11 +0200

    feat: Open new window on successful login based on logged user's role

--- Hospital/App.xaml.cs
+++ Hospital/App.xaml.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using Hospital.Views;
+using Hospital.Views.Nurse;
 
 namespace Hospital
 {
@@ -22,8 +24,29 @@ namespace Hospital
             {
                 if (loginView.IsVisible || !loginView.IsLoaded) return;
 
-                var mainView = new MainView();
-                mainView.Show();
+                var identityName = Thread.CurrentPrincipal.Identity.Name;
+                var role = identityName.Split("|")[1];
+
+                if (role == "PATIENT")
+                {
+                    throw new NotImplementedException();
+                }
+
+                else if (role == "NURSE")
+                {
+                    throw new NotImplementedException();
+                }
+
+                else if (role == "MANAGER")
+                {
+                    throw new NotImplementedException();
+                }
+
+                else if (role == "DOCTOR")
+                {
+                    throw new NotImplementedException();
+                }
+
                 loginView.Close();
             };
         }
