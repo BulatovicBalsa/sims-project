d0f2cb06f34cd4596eaa7ae5deb672681f1185c6
Vladimir Popov <vladapopov02@gmail.com>
2023-04-11 22:06:10 +0200

    feat: Add patients property inside NurseViewModel

---
+++ Hospital/ViewModels/Nurse/NurseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Models.Patient;
+using Hospital.Repositories.Patient;
+
+namespace Hospital.ViewModels.Nurse
+{
+    public class NurseViewModel : ViewModelBase
+    {
+        private List<Patient> _patients;
+
+        public List<Patient> Patients
+        {
+            get => _patients;
+            set
+            {
+                _patients = value;
+                OnPropertyChanged(nameof(Patients));
+            }
+        }
+
+        public NurseViewModel()
+        {
+            _patients = new PatientRepository().GetAll();
+        }
+    }
+}
