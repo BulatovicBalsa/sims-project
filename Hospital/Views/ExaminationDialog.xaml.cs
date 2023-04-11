005a156e35702ae72ba7e3a3845c4acc45f5aea6
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-04-11 23:01:54 +0200

    feat: Add Examination Dialog

---
+++ Hospital/Views/ExaminationDialog.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Hospital.Models.Doctor;
+using Hospital.Models.Examination;
+using Hospital.Models.Patient;
+using Hospital.Repositories.Examinaton;
+using Hospital.Repositories.Patient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hospital.Views
+{
+    /// <summary>
+    /// Interaction logic for ExaminationDialog.xaml
+    /// </summary>
+    public partial class ExaminationDialog : Window
+    {
+        private Doctor _doctor;
+        private ObservableCollection<Examination> _examinations; 
+        public ExaminationDialog(Doctor doctor, ObservableCollection<Examination> examinations)
+        {
+            InitializeComponent();
+
+            this._doctor = doctor;
+            this.PatientComboBox.ItemsSource = GetPatients();
+            this._examinations = examinations;
+        }
+
+        private List<Patient> GetPatients()
+        {
+            return new PatientRepository().GetAll();
+        }
+
+        private void AddExamination_Click(object sender, RoutedEventArgs e)
+        {
+            Patient? patient = PatientComboBox.SelectedItem as Patient;
+            if (patient == null)
+            {
+                MessageBox.Show("You must select patient");
+                return;
+            }
+
+            DateTime? startDate = ExaminationDatePicker.SelectedDate;
+            if (startDate == null)
+            {
+                MessageBox.Show("You must select date and time");
+                return;
+            }
+
+            bool? isOperationNullable = IsOperation.IsChecked;
+
+
+            Examination examination = new Examination(_doctor, patient, isOperationNullable.GetValueOrDefault(), startDate.GetValueOrDefault());
+
+            new ExaminationRepository(new ExaminationChangesTracker()).Add(examination, false);
+            _examinations.Add(examination);
+        }
+    }
+}
