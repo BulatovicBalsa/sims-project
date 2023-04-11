3fc92d7408501b308e8c4ee1c219a84d5701637c
Vladimir Popov <vladapopov02@gmail.com>
2023-04-11 22:51:15 +0200

    feat: Add Nurse view model with data bindings

--- Hospital/ViewModels/Nurse/NurseViewModel.cs
+++ Hospital/ViewModels/Nurse/NurseViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Hospital.Models.Patient;
 using Hospital.Repositories.Patient;
 
@@ -10,9 +13,21 @@ namespace Hospital.ViewModels.Nurse
 {
     public class NurseViewModel : ViewModelBase
     {
-        private List<Patient> _patients;
+        private ObservableCollection<Patient> _patients;
+        private PatientRepository _patientRepository;
+        private Patient _selectedPatient;
 
-        public List<Patient> Patients
+
+        public Patient SelectedPatient
+        {
+            get => _selectedPatient;
+            set
+            {
+                _selectedPatient = value;
+                OnPropertyChanged(nameof(SelectedPatient));
+            }
+        }
+        public ObservableCollection<Patient> Patients
         {
             get => _patients;
             set
@@ -24,7 +39,52 @@ namespace Hospital.ViewModels.Nurse
 
         public NurseViewModel()
         {
-            _patients = new PatientRepository().GetAll();
+            _patientRepository = new PatientRepository();
+            _patients = new ObservableCollection<Patient>(_patientRepository.GetAll());
+            SelectedPatient = null;
+
+            _patients.CollectionChanged += (sender, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                {
+                    if (e.NewItems != null)
+                    {
+                        foreach (var item in e.NewItems)
+                        {
+                            _patientRepository.Add((Patient) item);
+                        }
+                    }
+                }
+            };
+
+            DeletePatientCommand = new ViewModelCommand(ExecuteDeletePatientCommand, CanExecuteDeletePatientCommand);
+            UpdatePatientCommand = new ViewModelCommand(ExecuteUpdatePatientCommand, CanExecuteUpdatePatientCommand);
         }
+
+        public ICommand DeletePatientCommand { get; }
+        public ICommand UpdatePatientCommand { get; }
+
+        private bool CanExecuteDeletePatientCommand(object obj)
+        {
+            return SelectedPatient != null;
+        }
+
+        private void ExecuteDeletePatientCommand(object obj)
+        {
+            _patientRepository.Delete(SelectedPatient);
+            _patients.Remove(SelectedPatient);
+        }
+
+        private bool CanExecuteUpdatePatientCommand(object obj)
+        {
+            return SelectedPatient != null;
+        }
+
+        private void ExecuteUpdatePatientCommand(object obj)
+        {
+            _patientRepository.Update(SelectedPatient);
+        }
+
+
     }
 }
