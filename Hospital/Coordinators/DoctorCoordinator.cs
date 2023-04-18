2401c29183ababea8ce93cb3cd1cef57c1173ffa
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-04-18 18:02:16 +0200

    feat: Add GetViewedPatients and constructor for DoctorCoordinator

--- Hospital/Coordinators/DoctorCoordinator.cs
+++ Hospital/Coordinators/DoctorCoordinator.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Hospital.Models.Doctor;
+using Hospital.Models.Patient;
+using Hospital.Repositories.Examinaton;
+using Hospital.Repositories.Patient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,20 @@ namespace Hospital.Coordinators
 {
     public class DoctorCoordinator
     {
+        private readonly ExaminationRepository _examinationRepository;
+        private readonly PatientRepository _patientRepository;
+
+        public DoctorCoordinator(ExaminationRepository examinationRepository, PatientRepository patientRepository)
+        {
+            _examinationRepository = examinationRepository;
+            _patientRepository = patientRepository;
+        }
+
+        public List<Patient> GetViewedPatients(Doctor doctor)
+        {
+            var finishedExaminations = _examinationRepository.GetFinishedExaminations(doctor);
+            List<Patient> patients = finishedExaminations.Select(examination => examination.Patient).Distinct().ToList();
+            return patients;
+        }
     }
 }
