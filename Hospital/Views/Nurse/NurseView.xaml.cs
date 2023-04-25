d1fc4d55756f791b3f4f7b96d984e7ee037a220e
Vladimir Popov <vladapopov02@gmail.com>
2023-04-25 15:22:55 +0200

    refactor: Cleanup imports and code

--- Hospital/Views/Nurse/NurseView.xaml.cs
+++ Hospital/Views/Nurse/NurseView.xaml.cs
@@ -1,63 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Interop;
 
+namespace Hospital.Views.Nurse;
 
-namespace Hospital.Views.Nurse
+/// <summary>
+///     Interaction logic for NurseView.xaml
+/// </summary>
+public partial class NurseView : Window
 {
-    /// <summary>
-    /// Interaction logic for NurseView.xaml
-    /// </summary>
-    public partial class NurseView : Window
+    public NurseView()
     {
-        public NurseView()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        [DllImport("user32.dll")]
-        private static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+    [DllImport("user32.dll")]
+    private static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
 
-        private void ControlBar_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var helper = new WindowInteropHelper(this);
-            SendMessage(helper.Handle, 161, 2, 0);
-        }
+    private void ControlBar_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        var helper = new WindowInteropHelper(this);
+        SendMessage(helper.Handle, 161, 2, 0);
+    }
 
-        private void ControlBar_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
-        }
+    private void ControlBar_OnMouseEnter(object sender, MouseEventArgs e)
+    {
+        MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+    }
 
-        private void BtnMinimize_OnClick(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
+    private void BtnMinimize_OnClick(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
 
-        private void BtnClose_OnClick(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
+    private void BtnClose_OnClick(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
 
-        private void BtnMaximize_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (WindowState == WindowState.Normal)
-            {
-                WindowState = WindowState.Maximized;
-            }
-            else this.WindowState = WindowState.Normal;
-        }
+    private void BtnMaximize_OnClick(object sender, RoutedEventArgs e)
+    {
+        WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
     }
 }
