44daa2a0d6cbc1f58b7a5658053545cebc8a744c
Vladimir Popov <vladapopov02@gmail.com>
2023-04-28 01:12:17 +0200

    refactor: Decluttered App.xaml.cs code

--- Hospital/App.xaml.cs
+++ Hospital/App.xaml.cs
@@ -1,73 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using Hospital.Models.Doctor;
 using Hospital.Repositories.Doctor;
-using Hospital.Views;
 using Hospital.Repositories.Patient;
 using Hospital.Views;
 using Hospital.Views.Manager;
 using Hospital.Views.Nurse;
 
-namespace Hospital
+namespace Hospital;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    protected void ApplicationStart(object sender, EventArgs e)
     {
-        protected void ApplicationStart(object sender, EventArgs e)
+        var loginView = new LoginView();
+        loginView.Show();
+        loginView.IsVisibleChanged += (s, ev) =>
         {
-            var loginView = new LoginView();
-            loginView.Show();
-            loginView.IsVisibleChanged += (s, ev) =>
+            if (loginView.IsVisible || !loginView.IsLoaded) return;
+
+            var identityName = Thread.CurrentPrincipal.Identity.Name;
+            var id = identityName.Split("|")[0];
+            var role = identityName.Split("|")[1];
+
+            if (role == "PATIENT")
             {
-                if (loginView.IsVisible || !loginView.IsLoaded) return;
-
-                var identityName = Thread.CurrentPrincipal.Identity.Name;
-                var id = identityName.Split("|")[0];
-                var role = identityName.Split("|")[1];
-
-                if (role == "PATIENT")
+                var patient = new PatientRepository().GetById(id);
+                if (patient == null)
                 {
-                    var patient = new PatientRepository().GetById(id);
-                    if (patient == null)
-                    {
-                        MessageBox.Show("Login was not successful.");
-                        return;
-                    }
-                    var patientView = new PatientView(patient);
-                    patientView.Show();
+                    MessageBox.Show("Login was not successful.");
+                    return;
                 }
 
-                else if (role == "NURSE")
-                {
-                    var nurseView = new NurseView();
-                    nurseView.Show();
-                }
+                var patientView = new PatientView(patient);
+                patientView.Show();
+            }
 
-                else if (role == "MANAGER")
-                {
-                    var managerView = new ManagerView();
-                    managerView.Show();
-                    
-                    //throw new NotImplementedException();
-                }
+            else if (role == "NURSE")
+            {
+                var nurseView = new NurseMainView();
+                nurseView.Show();
+            }
 
-                else if (role == "DOCTOR")
-                {
-                    var doctor = new DoctorRepository().GetById(id);
-                    DoctorView doctorView = new DoctorView(doctor);
-                    doctorView.Show();
-                }
+            else if (role == "MANAGER")
+            {
+                var managerView = new ManagerView();
+                managerView.Show();
+            }
 
-                loginView.Close();
-            };
-        }
+            else if (role == "DOCTOR")
+            {
+                var doctor = new DoctorRepository().GetById(id);
+                var doctorView = new DoctorView(doctor);
+                doctorView.Show();
+            }
+
+            loginView.Close();
+        };
     }
 }
