8f536ead0314891798eb5e316f7d3e962de11ed3
Vladimir Popov <vladapopov02@gmail.com>
2023-04-30 22:33:33 +0200

    refactor: Cleanup NurseRepository code

--- Hospital/Repositories/Nurse/NurseRepository.cs
+++ Hospital/Repositories/Nurse/NurseRepository.cs
@@ -1,59 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Hospital.Exceptions;
 using Hospital.Serialization;
 
-namespace Hospital.Repositories.Nurse
+namespace Hospital.Repositories.Nurse;
+
+public class NurseRepository
 {
-    using Hospital.Models.Nurse;
+    private const string FilePath = "../../../Data/nurses.csv";
 
-    public class NurseRepository
+    public List<Models.Nurse.Nurse> GetAll()
     {
-        private const string FilePath = "../../../Data/nurses.csv";
+        return Serializer<Models.Nurse.Nurse>.FromCSV(FilePath);
+    }
 
-        public List<Nurse> GetAll()
-        {
-            return Serializer<Nurse>.FromCSV(FilePath);
-        }
+    public Models.Nurse.Nurse? GetById(string id)
+    {
+        return GetAll().FirstOrDefault(nurse => nurse.Id == id);
+    }
 
-        public Nurse? GetById(string id)
-        {
-            return GetAll().FirstOrDefault(nurse => nurse.Id == id);
-        }
+    public Models.Nurse.Nurse? GetByUsername(string username)
+    {
+        return GetAll().FirstOrDefault(nurse => nurse.Profile.Username == username);
+    }
 
-        public Nurse? GetByUsername(string username)
-        {
-            return GetAll().FirstOrDefault(nurse => nurse.Profile.Username == username);
-        }
+    public void Add(Models.Nurse.Nurse nurse)
+    {
+        var allNurses = GetAll();
+        allNurses.Add(nurse);
+        Serializer<Models.Nurse.Nurse>.ToCSV(allNurses, FilePath);
+    }
 
-        public void Add(Nurse nurse)
-        {
-            var allNurses = GetAll();
-            allNurses.Add(nurse);
-            Serializer<Nurse>.ToCSV(allNurses, FilePath);
-        }
+    public void Update(Models.Nurse.Nurse nurse)
+    {
+        var allNurses = GetAll();
 
-        public void Update(Nurse nurse)
-        {
-            var allNurses= GetAll();
+        var indexToUpdate = allNurses.FindIndex(nurseRecord => nurseRecord.Id == nurse.Id);
+        if (indexToUpdate == -1)
+            throw new ObjectNotFoundException($"Nurse with id {nurse.Id} was not found.");
+        allNurses[indexToUpdate] = nurse;
 
-            var indexToUpdate = allNurses.FindIndex(nurseRecord => nurseRecord.Id == nurse.Id);
-            if (indexToUpdate == -1)
-                throw new KeyNotFoundException($"Nurse with id {nurse.Id} was not found.");
-            allNurses[indexToUpdate] = nurse;
+        Serializer<Models.Nurse.Nurse>.ToCSV(allNurses, FilePath);
+    }
 
-            Serializer<Nurse>.ToCSV(allNurses, FilePath);
-        }
-        public void Delete(Nurse nurse)
-        {
-            var allNurses = GetAll();
+    public void Delete(Models.Nurse.Nurse nurse)
+    {
+        var allNurses = GetAll();
 
-            if (!allNurses.Remove(nurse))
-                throw new KeyNotFoundException($"Nurse with id {nurse.Id} was not found.");
+        if (!allNurses.Remove(nurse))
+            throw new ObjectNotFoundException($"Nurse with id {nurse.Id} was not found.");
 
-            Serializer<Nurse>.ToCSV(allNurses, FilePath);
-        }
+        Serializer<Models.Nurse.Nurse>.ToCSV(allNurses, FilePath);
     }
 }
