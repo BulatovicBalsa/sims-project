e1b9812fc4287e684e288b9bdf5f04dad17670dd
MomirMilutinovic <milutinm462@gmail.com>
2023-05-02 03:19:34 -0700

    feat: Add equipment order class

---
+++ Hospital/Models/Manager/EquipmentOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hospital.Models.Manager
+{
+    public class EquipmentOrder
+    {
+        public List<EquipmentOrderItem> items;
+
+        public EquipmentOrder()
+        {
+            items = new List<EquipmentOrderItem>();
+        }
+
+        public void AddOrUpdateItem(Equipment equipment, uint amount)
+        {
+            EquipmentOrderItem item = items.Find(e => e.EquipmentId == equipment.Id);
+            if (item == null)
+                items.Add(new EquipmentOrderItem(amount, equipment.Id, equipment));
+            else
+                item.Amount = amount;
+        }
+    }
+}
---
+++ Hospital/Models/Manager/EquipmentOrderItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Hospital.Models.Manager;
+
+public class EquipmentOrderItem
+{
+    public uint Amount { get; set; }
+    public string EquipmentId { get; set; }
+
+    public string OrderId { get; set; }
+
+    public Equipment Equipment { get; set; }
+
+    public EquipmentOrderItem(uint amount, string equipmentId, Equipment equipment)
+    {
+        Amount = amount;
+        EquipmentId = equipmentId;
+        OrderId = System.Guid.NewGuid().ToString();
+        Equipment = equipment;
+    }
+}
