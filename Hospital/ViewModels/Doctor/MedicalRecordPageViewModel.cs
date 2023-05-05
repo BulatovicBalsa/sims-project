5f2abb06ce8fde35a2a817abd7e8d572d7c003bd
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-05 15:57:34 +0200

    refactor: Refactor Medical Record Page view model

--- Hospital/ViewModels/Doctor/MedicalRecordPageViewModel.cs
+++ Hospital/ViewModels/Doctor/MedicalRecordPageViewModel.cs
@@ -12,239 +12,273 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Navigation;
 
-namespace Hospital.ViewModels
+namespace Hospital.ViewModels;
+
+public class MedicalRecordPageViewModel : ViewModelBase
 {
-    public class MedicalRecordPageViewModel : ViewModelBase
+    private readonly DoctorService _doctorService = new();
+
+    private ObservableCollection<string> _allergies;
+
+    private string _height;
+
+    private ObservableCollection<string> _medicalHistory;
+
+    private Patient _patient;
+
+    private string _selectedAllergy;
+
+    private string _selectedMedicalCondition;
+
+    private string _weight;
+
+
+    public MedicalRecordPageViewModel(Patient patient)
     {
+        Patient = patient;
+        Weight = Patient.MedicalRecord.Weight.ToString();
+        Height = Patient.MedicalRecord.Height.ToString();
+        Allergies = new ObservableCollection<string>(patient.MedicalRecord.Allergies);
+        MedicalHistory = new ObservableCollection<string>(patient.MedicalRecord.MedicalHistory);
 
-        private readonly DoctorService _doctorService = new DoctorService();
+        AddAllergyCommand = new RelayCommand(AddAllergy);
+        DeleteAllergyCommand = new RelayCommand(DeleteAllergy);
+        UpdateAllergyCommand = new RelayCommand(UpdateAllergy);
 
-        private Patient _patient;
+        AddMedicalConditionCommand = new RelayCommand(AddMedicalCondition);
+        DeleteMedicalConditionCommand = new RelayCommand(DeleteMedicalCondition);
+        UpdateMedicalConditionCommand = new RelayCommand(UpdateMedicalCondition);
 
-        public Patient Patient
+        ChangeHeightCommand = new RelayCommand(ChangeHeight);
+        ChangeWeightCommand = new RelayCommand(ChangeWeight);
+    }
+
+    public Patient Patient
+    {
+        get => _patient;
+        set
         {
-            get { return _patient; }
-            set { _patient = value; OnPropertyChanged(nameof(Patient));}
+            _patient = value;
+            OnPropertyChanged(nameof(Patient));
+        }
+    }
+
+    public string Height //Maybe rename it
+    {
+        get => _height;
+        set
+        {
+            _height = value;
+            OnPropertyChanged(nameof(Height));
+        }
+    }
+
+    public string Weight
+    {
+        get => _weight;
+        set
+        {
+            _weight = value;
+            OnPropertyChanged(nameof(Weight));
+        }
+    }
+
+    public string SelectedAllergy
+    {
+        get => _selectedAllergy;
+        set
+        {
+            _selectedAllergy = value;
+            OnPropertyChanged(nameof(SelectedAllergy));
+        }
+    }
+
+    public string SelectedMedicalCondition
+    {
+        get => _selectedMedicalCondition;
+        set
+        {
+            _selectedMedicalCondition = value;
+            OnPropertyChanged(nameof(SelectedMedicalCondition));
+        }
+    }
+
+    public ObservableCollection<string> MedicalHistory
+    {
+        get => _medicalHistory;
+        set
+        {
+            _medicalHistory = value;
+            OnPropertyChanged(nameof(MedicalHistory));
+        }
+    }
+
+    public ObservableCollection<string> Allergies
+    {
+        get => _allergies;
+        set
+        {
+            _allergies = value;
+            OnPropertyChanged(nameof(Allergies));
+        }
+    }
+
+
+    public ICommand AddAllergyCommand { get; set; }
+    public ICommand DeleteAllergyCommand { get; set; }
+    public ICommand UpdateAllergyCommand { get; set; }
+    public ICommand AddMedicalConditionCommand { get; set; }
+    public ICommand UpdateMedicalConditionCommand { get; set; }
+    public ICommand DeleteMedicalConditionCommand { get; set; }
+    public ICommand ChangeHeightCommand { get; set; }
+    public ICommand ChangeWeightCommand { get; set; }
+
+    private void AddAllergy()
+    {
+        AddHealthCondition(HealthConditionType.Allergy);
+    }
+
+    private void AddMedicalCondition()
+    {
+        AddHealthCondition(HealthConditionType.MedicalCondition);
+    }
+
+    private void UpdateAllergy()
+    {
+        UpdateHealthCondition(HealthConditionType.Allergy);
+    }
+
+    private void UpdateMedicalCondition()
+    {
+        UpdateHealthCondition(HealthConditionType.MedicalCondition);
+    }
+
+    private void DeleteMedicalCondition()
+    {
+        DeleteHealthCondition(HealthConditionType.MedicalCondition);
+    }
+
+    private void DeleteAllergy()
+    {
+        DeleteHealthCondition(HealthConditionType.Allergy);
+    }
+
+    private void ChangeWeight()
+    {
+        ChangePhysicalCharacteristic(false);
+    }
+
+    private void ChangeHeight()
+    {
+        ChangePhysicalCharacteristic(true);
+    }
+
+    private void AddHealthCondition(HealthConditionType conditionType)
+    {
+        Action<string> medicalRecordOperation = conditionType == HealthConditionType.Allergy
+            ? Patient.MedicalRecord.AddAllergy
+            : Patient.MedicalRecord.AddMedicalConidition;
+
+        var conditionToAdd = Interaction.InputBox($"Insert {conditionType}: ", $"Add {conditionType}");
+        try
+        {
+            medicalRecordOperation(conditionToAdd);
+        }
+        catch (ArgumentException ex)
+        {
+            MessageBox.Show(ex.Message);
+            return;
         }
 
-        private string _height;
+        _doctorService.UpdatePatient(Patient);
+        RefreshHealthConditionCollection(conditionType);
+    }
 
-        public string Height //Maybe rename it
+    private void UpdateHealthCondition(HealthConditionType conditionType)
+    {
+        Action<string, string> medicalRecordOperation = conditionType == HealthConditionType.Allergy
+            ? Patient.MedicalRecord.UpdateAllergy
+            : Patient.MedicalRecord.UpdateMedicalCondition;
+        var selectedHealthCondition =
+            conditionType == HealthConditionType.Allergy ? SelectedAllergy : SelectedMedicalCondition;
+
+        var selectedCondition = (string)selectedHealthCondition;
+        if (selectedCondition is null)
         {
-            get { return _height; }
-            set { _height = value; OnPropertyChanged(nameof(Height)); }
+            MessageBox.Show("You must select condition in order to update it");
+            return;
         }
 
-        private string _weight;
-
-        public string Weight
+        var updatedCondition = Interaction.InputBox($"Update '{selectedCondition}' name: ", $"Update {conditionType}");
+        try
         {
-            get { return _weight; }
-            set { _weight = value; OnPropertyChanged(nameof(Weight)); }
+            medicalRecordOperation(selectedCondition, updatedCondition);
+        }
+        catch (ArgumentException ex)
+        {
+            MessageBox.Show(ex.Message);
+            return;
         }
 
-        private string _selectedAllergy;
+        _doctorService.UpdatePatient(Patient);
+        RefreshHealthConditionCollection(conditionType);
+    }
 
-        public string SelectedAllergy
+    private void DeleteHealthCondition(HealthConditionType conditionType)
+    {
+        Action<string> medicalRecordOperation = conditionType == HealthConditionType.Allergy
+            ? Patient.MedicalRecord.DeleteAllergy
+            : Patient.MedicalRecord.DeleteMedicalCondition;
+        var selectedHealthCondition =
+            conditionType == HealthConditionType.Allergy ? SelectedAllergy : SelectedMedicalCondition;
+
+        var selectedCondition = (string)selectedHealthCondition;
+        if (selectedCondition is null)
         {
-            get { return _selectedAllergy; }
-            set { _selectedAllergy = value; OnPropertyChanged(nameof(SelectedAllergy)); }
+            MessageBox.Show($"You must select {conditionType} in order to delete it");
+            return;
         }
 
-        private string _selectedMedicalCondition;
-
-        public string SelectedMedicalCondition
+        try
         {
-            get { return _selectedMedicalCondition; }
-            set { _selectedMedicalCondition = value; OnPropertyChanged(nameof(SelectedMedicalCondition)); }
+            medicalRecordOperation(selectedCondition);
+        }
+        catch (ArgumentException ex)
+        {
+            MessageBox.Show(ex.Message);
+            return;
         }
 
-        private ObservableCollection<string> _medicalHistory;
+        _doctorService.UpdatePatient(Patient);
+        RefreshHealthConditionCollection(conditionType);
+    }
 
-        public ObservableCollection<string> MedicalHistory
+    private void ChangePhysicalCharacteristic(bool isHeight)
+    {
+        Action<int> medicalRecordOperation =
+            isHeight ? Patient.MedicalRecord.ChangeHeight : Patient.MedicalRecord.ChangeWeight;
+        var sizeToChangeStr = isHeight ? Height : Weight;
+
+        var sizeToChange = int.Parse(sizeToChangeStr);
+        try
         {
-            get { return _medicalHistory; }
-            set { _medicalHistory = value; OnPropertyChanged(nameof(MedicalHistory)); }
+            medicalRecordOperation(sizeToChange);
+        }
+        catch (ArgumentException ex)
+        {
+            MessageBox.Show(ex.Message);
+            return;
         }
 
-        private ObservableCollection<string> _allergies;
+        _doctorService.UpdatePatient(Patient);
+        MessageBox.Show("Succeed");
+    }
 
-        public ObservableCollection<string> Allergies
-        {
-            get { return _allergies; }
-            set { _allergies = value; OnPropertyChanged(nameof(Allergies)); }
-        }
-
-
-        public ICommand AddAllergyCommand { get; set; }
-        public ICommand DeleteAllergyCommand { get; set; }
-        public ICommand UpdateAllergyCommand { get; set; }
-        public ICommand AddMedicalConditionCommand { get; set; }
-        public ICommand UpdateMedicalConditionCommand { get; set; }
-        public ICommand DeleteMedicalConditionCommand { get; set; }
-        public ICommand ChangeHeightCommand { get; set; }
-        public ICommand ChangeWeightCommand { get; set; }
-
-
-        public MedicalRecordPageViewModel(Patient patient)
-        {
-            Patient = patient;
-            Weight = Patient.MedicalRecord.Weight.ToString();
-            Height = Patient.MedicalRecord.Height.ToString();
-            Allergies = new ObservableCollection<string>(patient.MedicalRecord.Allergies);
-            MedicalHistory = new ObservableCollection<string>(patient.MedicalRecord.MedicalHistory);
-
-            AddAllergyCommand = new RelayCommand(AddAllergyButton_Click);
-            DeleteAllergyCommand = new RelayCommand(DeleteAllergyButton_Click);
-            UpdateAllergyCommand = new RelayCommand(UpdateAllergyButton_Click);
-
-            AddMedicalConditionCommand = new RelayCommand(AddMedicalConditionButton_Click);
-            DeleteMedicalConditionCommand = new RelayCommand(DeleteMedicalConditionButton_Click);
-            UpdateMedicalConditionCommand = new RelayCommand(UpdateMedicalConditionButton_Click);
-
-            ChangeHeightCommand = new RelayCommand(ChangeHeightButton_Click);
-            ChangeWeightCommand = new RelayCommand(ChangeWeightButton_Click);
-        }
-
-        private void AddAllergyButton_Click()
-        {
-            addHealthCondition(HealthConditionType.Allergy);
-        }
-
-        private void AddMedicalConditionButton_Click()
-        {
-            addHealthCondition(HealthConditionType.MedicalCondition);
-        }
-
-        private void UpdateAllergyButton_Click()
-        {
-            updateHealthCondition(HealthConditionType.Allergy);
-        }
-
-        private void UpdateMedicalConditionButton_Click()
-        {
-            updateHealthCondition(HealthConditionType.MedicalCondition);
-        }
-
-        private void DeleteMedicalConditionButton_Click()
-        {
-            deleteHealthCondition(HealthConditionType.MedicalCondition);
-        }
-
-        private void DeleteAllergyButton_Click()
-        {
-            deleteHealthCondition(HealthConditionType.Allergy);
-        }
-
-        private void ChangeWeightButton_Click()
-        {
-            changePhysicalCharacteristic(false);
-        }
-
-        private void ChangeHeightButton_Click()
-        {
-            changePhysicalCharacteristic(true);
-        }
-
-        private void addHealthCondition(HealthConditionType conditionType)
-        {
-            Action<string> medicalRecordOperation = conditionType == HealthConditionType.Allergy ? Patient.MedicalRecord.AddAllergy : Patient.MedicalRecord.AddMedicalConidition;
-            var healthConditionCollection = conditionType == HealthConditionType.Allergy ? Allergies : MedicalHistory;
-
-            string conditionToAdd = Interaction.InputBox($"Insert {conditionType}: ", $"Add {conditionType}", "");
-            try
-            {
-                medicalRecordOperation(conditionToAdd);
-            }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show(ex.Message);
-                return;
-            }
-
-            _doctorService.UpdatePatient(Patient);
-            refreshHealthConditionCollection(conditionType);
-        }
-
-        private void updateHealthCondition(HealthConditionType conditionType)
-        {
-            Action<string, string> medicalRecordOperation = conditionType == HealthConditionType.Allergy ? Patient.MedicalRecord.UpdateAllergy : Patient.MedicalRecord.UpdateMedicalCondition;
-            var selectedHealthCondition = conditionType == HealthConditionType.Allergy ? SelectedAllergy : SelectedMedicalCondition;
-            var healthConditionCollection = conditionType == HealthConditionType.Allergy ? Allergies : MedicalHistory;
-
-            string? selectedCondition = (string)selectedHealthCondition;
-            if (selectedCondition == null)
-            {
-                MessageBox.Show("You must select condition in order to update it");
-                return;
-            }
-            string updatedCondition = Interaction.InputBox($"Update '{selectedCondition}' name: ", $"Update {conditionType}", "");
-            try
-            {
-                medicalRecordOperation(selectedCondition, updatedCondition);
-            }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show(ex.Message);
-                return;
-            }
-
-            _doctorService.UpdatePatient(Patient);
-            refreshHealthConditionCollection(conditionType);
-            
-        }
-
-        private void deleteHealthCondition(HealthConditionType conditionType)
-        {
-            Action<string> medicalRecordOperation = conditionType == HealthConditionType.Allergy ? Patient.MedicalRecord.DeleteAllergy : Patient.MedicalRecord.DeleteMedicalCondition;
-            var selectedHealthCondition = conditionType == HealthConditionType.Allergy ? SelectedAllergy : SelectedMedicalCondition;
-            var healthConditionCollection = conditionType == HealthConditionType.Allergy ? Allergies : MedicalHistory;
-
-            string? selectedCondition = (string)selectedHealthCondition;
-            if (selectedCondition == null)
-            {
-                MessageBox.Show($"You must select {conditionType} in order to delete it");
-                return;
-            }
-            try
-            {
-                medicalRecordOperation(selectedCondition);
-            }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show(ex.Message);
-                return;
-            }
-
-            _doctorService.UpdatePatient(Patient);
-            refreshHealthConditionCollection(conditionType);
-        }
-
-        private void changePhysicalCharacteristic(bool isHeight)
-        {
-            Action<int> medicalRecordOperation = isHeight ? Patient.MedicalRecord.ChangeHeight : Patient.MedicalRecord.ChangeWeight;
-            var sizeToChangeStr = isHeight ? Height : Weight;
-
-            int sizeToChange = Int32.Parse(sizeToChangeStr);
-            try
-            {
-                medicalRecordOperation(sizeToChange);
-            }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show(ex.Message);
-                return;
-            }
-            _doctorService.UpdatePatient(Patient);
-            MessageBox.Show("Succeed");
-        }
-
-        private void refreshHealthConditionCollection(HealthConditionType conditionType)
-        {
-            if (conditionType == HealthConditionType.Allergy)
-                Allergies = new ObservableCollection<string>(Patient.MedicalRecord.Allergies);
-            else
-                MedicalHistory = new ObservableCollection<string>(Patient.MedicalRecord.MedicalHistory);
-        }
+    private void RefreshHealthConditionCollection(HealthConditionType conditionType)
+    {
+        if (conditionType == HealthConditionType.Allergy)
+            Allergies = new ObservableCollection<string>(Patient.MedicalRecord.Allergies);
+        else
+            MedicalHistory = new ObservableCollection<string>(Patient.MedicalRecord.MedicalHistory);
     }
 }
