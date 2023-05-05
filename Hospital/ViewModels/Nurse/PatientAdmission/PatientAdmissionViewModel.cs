dc235eecc9fb41caf5848eb3f1abcb280dcbd290
Vladimir Popov <vladapopov02@gmail.com>
2023-05-05 18:44:58 +0200

    feat: Add ExecuteStartAdmissionCommand

--- Hospital/ViewModels/Nurse/PatientAdmission/PatientAdmissionViewModel.cs
+++ Hospital/ViewModels/Nurse/PatientAdmission/PatientAdmissionViewModel.cs
@@ -1,27 +1,37 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
 using Hospital.Models.Patient;
 using Hospital.Repositories.Patient;
+using Hospital.Services;
 using Hospital.ViewModels.Nurse.Patients;
+using Hospital.Views.Nurse.PatientAdmission;
 using Hospital.Views.Nurse.Patients;
 
 namespace Hospital.ViewModels.Nurse.PatientAdmission;
 
 public class PatientAdmissionViewModel : ViewModelBase
 {
+    private readonly ExaminationService _examinationService;
     private readonly PatientRepository _patientRepository;
     private ObservableCollection<Patient> _patients;
     private Patient? _selectedPatient;
 
     public PatientAdmissionViewModel()
     {
+        _examinationService = new ExaminationService();
         _patientRepository = new PatientRepository();
         _patients = new ObservableCollection<Patient>(_patientRepository.GetAll());
 
         _patientRepository.PatientAdded += patient => { _patients.Add(patient); };
+        _patientRepository.PatientUpdated += patient =>
+        {
+            _patients.Remove(patient);
+            _patients.Add(patient);
+        };
 
         AddPatientCommand = new ViewModelCommand(ExecuteAddPatientCommand);
+        StartAdmissionCommand = new ViewModelCommand(ExecuteStartAdmissionCommand, IsPatientSelected);
     }
 
     public Patient? SelectedPatient
@@ -61,7 +71,20 @@ public class PatientAdmissionViewModel : ViewModelBase
 
     private void ExecuteStartAdmissionCommand(object obj)
     {
-        throw new NotImplementedException();
+        var admissibleExamination = _examinationService.GetAdmissibleExamination(SelectedPatient);
+
+        if (admissibleExamination == null)
+        {
+            MessageBox.Show("Selected patient does not have an examination in the next 15 minutes", "Error");
+            return;
+        }
+
+        var patientAdmissionDialog = new AdmissionDialogView
+        {
+            DataContext = new AdmissionDialogViewModel(_patientRepository, SelectedPatient, admissibleExamination)
+        };
+
+        patientAdmissionDialog.ShowDialog();
     }
 
     private bool IsPatientSelected(object obj)
