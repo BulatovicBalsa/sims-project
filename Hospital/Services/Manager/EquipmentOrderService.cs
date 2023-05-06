353bee606da6a2482b86643b11b682764abe0df6
MomirMilutinovic <milutinm462@gmail.com>
2023-05-06 04:23:26 -0700

    feat: EquipmentOrderService saves changes to file

--- Hospital/Services/Manager/EquipmentOrderService.cs
+++ Hospital/Services/Manager/EquipmentOrderService.cs
@@ -34,8 +34,22 @@ public class EquipmentOrderService
     {
         foreach (var order in EquipmentOrderRepository.Instance.GetAll())
         {
+            var warehouse = RoomRepository.Instance.GetWarehouse();
             order.PickUp(RoomRepository.Instance.GetWarehouse());
             EquipmentOrderRepository.Instance.Update(order);
+            
+            foreach (var equipmentPlacement in warehouse.Equipment)
+            {
+                //TODO: Perhaps don't use try catch. Use GetByKeyInstead
+                try
+                {
+                    EquipmentPlacementRepository.Instance.Update(equipmentPlacement);
+                }
+                catch (KeyNotFoundException)
+                {
+                    EquipmentPlacementRepository.Instance.Add(equipmentPlacement);
+                }
+            }
         }
     }
 }
