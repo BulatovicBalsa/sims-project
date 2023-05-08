9c83431ce63ba6d21df39aca9dabaed57fa53fbb
Vladimir Popov <vladapopov02@gmail.com>
2023-05-08 23:44:31 +0200

    feat: Add a dialog where user can choose which examination to postpone

---
+++ Hospital/ViewModels/Nurse/UrgentExaminations/PostponeExaminationViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Hospital.Models.Examination;
+
+namespace Hospital.ViewModels.Nurse.UrgentExaminations
+{
+    public class PostponeExaminationViewModel : ViewModelBase
+    {
+        private Examination _selectedExamination;
+
+        public PostponeExaminationViewModel()
+        {
+            // dummy constructor
+        }
+
+        public PostponeExaminationViewModel(List<Examination> examinations)
+        {
+            Examinations = examinations;
+            _selectedExamination = null;
+        }
+
+        public List<Examination> Examinations { get; }
+        public Examination SelectedExamination
+        {
+            get => _selectedExamination;
+            set
+            {
+                _selectedExamination = value;
+                OnPropertyChanged(nameof(SelectedExamination));
+            }
+        }
+
+        public ICommand PostponeExaminationCommand { get; }
+        public ICommand CloseDialogCommand { get; }
+    }
+}
