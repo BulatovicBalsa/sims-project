4be41f52dbed5d623288b6042a45e4a95188696a
Vladimir Popov <vladapopov02@gmail.com>
2023-05-09 01:48:14 +0200

    feat: Add postpone examination function

--- Hospital/ViewModels/Nurse/UrgentExaminations/UrgentExaminationsViewModel.cs
+++ Hospital/ViewModels/Nurse/UrgentExaminations/UrgentExaminationsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Input;
 using Hospital.Coordinators;
@@ -11,6 +12,7 @@ using Hospital.Repositories.Doctor;
 using Hospital.Repositories.Examinaton;
 using Hospital.Repositories.Patient;
 using Hospital.Services;
+using Hospital.Views.Nurse.UrgentExaminations;
 
 namespace Hospital.ViewModels.Nurse.UrgentExaminations;
 
@@ -97,8 +99,49 @@ public class UrgentExaminationsViewModel : ViewModelBase
             return;
         }
 
-        MessageBox.Show("Error happened", "Error");
+        // nema u naredna dva sata
+        var doctorFirstFreeTimeslot = suitableDoctors
+            .Select(doctor => new KeyValuePair<Doctor, DateTime>(doctor, _timeslotService.GetEarliestFreeTimeslot(doctor))).ToList();
+        var firstFreeTimeslot = doctorFirstFreeTimeslot.Min(doctorTimeslotPair => doctorTimeslotPair.Value);
+        var firstFreeDoctorTimeslotPair =
+            doctorFirstFreeTimeslot.Where(doctorTimeslotPair => doctorTimeslotPair.Value == firstFreeTimeslot);
 
+        doctorFirstFreeTimeslot = doctorFirstFreeTimeslot.OrderBy(pair => pair.Value).ToList();
+        var postponeableExaminations = new List<Examination>();
+
+        while (postponeableExaminations.Count < 5)
+        {
+            foreach (var doctorTimeslotPair in doctorFirstFreeTimeslot)
+            {
+                var doctorExaminations = _examinationRepository.GetAll(doctorTimeslotPair.Key).OrderBy(examination => examination.Start)
+                    .Where(examination => examination.Start > DateTime.Now && !examination.Urgent).ToList();
+
+                postponeableExaminations.InsertRange(0, doctorExaminations);
+            }
+        }
+
+        if (postponeableExaminations.Count > 5)
+            postponeableExaminations = postponeableExaminations.Take(5).ToList();
+
+        if (postponeableExaminations.Count == 0)
+        {
+            MessageBox.Show("There are no examinations to postpone", "Error");
+            return;
+        }
+
+        var postponeExaminationViewModel = new PostponeExaminationViewModel(postponeableExaminations, doctorFirstFreeTimeslot);
+        postponeExaminationViewModel.DialogClosed += (cancelled, newTimeslot, freeDoctor) =>
+        {
+            if (cancelled)
+                return;
+
+            _examinationRepository.Add(new Examination(freeDoctor, SelectedPatient, IsOperation, newTimeslot ?? DateTime.MinValue, null), false);
+        };
+        var postponeExaminationDialog = new PostponeExaminationDialogView()
+        {
+            DataContext = postponeExaminationViewModel
+        };
+        postponeExaminationDialog.ShowDialog();
     }
 
     private bool CanExecuteCreateUrgentExaminationCommand(object obj)
