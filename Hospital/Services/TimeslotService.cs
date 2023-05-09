d5ba99aad952fa24920c78aeec304d256d311a36
Vladimir Popov <vladapopov02@gmail.com>
2023-05-09 03:45:12 +0200

    refactor: Cleanup TimeslotService

--- Hospital/Services/TimeslotService.cs
+++ Hospital/Services/TimeslotService.cs
@@ -1,71 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Hospital.Models.Doctor;
-using Hospital.Repositories.Doctor;
 using Hospital.Repositories.Examinaton;
 
-namespace Hospital.Services
+namespace Hospital.Services;
+
+public class TimeslotService
 {
-    public class TimeslotService
+    private readonly ExaminationRepository _examinationRepository;
+
+    public TimeslotService()
     {
-        private readonly DoctorRepository _doctorRepository;
-        private readonly ExaminationRepository _examinationRepository;
-
-        public TimeslotService()
-        {
-            _doctorRepository = new DoctorRepository();
-            _examinationRepository = new ExaminationRepository();
-        }
-
-        public DateTime? GetEarliestFreeTimeslotIn2Hours(Doctor doctor)
-        {
-            var upcomingDoctorExaminations =  _examinationRepository.GetAll(doctor).Where(examination => examination.Start > DateTime.Now).ToList();
-            var lowerTimeBound = NormalizeTime(DateTime.Now);
-            var upperTimeBound = lowerTimeBound.AddHours(2);
-
-            for (var time = lowerTimeBound; time <= upperTimeBound; time = time.AddMinutes(15))
-            {
-                if (upcomingDoctorExaminations.Any(examination => CompareDates(examination.Start, time)))
-                {
-                    continue;
-                }
-
-                return time;
-            }
-
-            return null;
-        }
-
-        public DateTime GetEarliestFreeTimeslot(Doctor doctor)
-        {
-            var upcomingDoctorExaminations = _examinationRepository.GetAll(doctor).Where(examination => examination.Start > DateTime.Now).ToList();
-            var lowerTimeBound = NormalizeTime(DateTime.Now);
-
-            for (var time = lowerTimeBound;; time = time.AddMinutes(15))
-            {
-                if (!upcomingDoctorExaminations.Any(examination => CompareDates(examination.Start, time)))
-                {
-                    return time;
-                }
-            }
-        }
-
-        private DateTime NormalizeTime(DateTime time)
-        {
-            var validExaminationTimestamp = time.AddSeconds(-time.Second).AddMilliseconds(-time.Millisecond);
-            return validExaminationTimestamp.Minute % 15 == 0 ? validExaminationTimestamp : validExaminationTimestamp.AddMinutes(15 - validExaminationTimestamp.Minute % 15);
-        }
-
-        private bool CompareDates(DateTime dateTime1, DateTime dateTime2)
-        {
-            return dateTime1.Date == dateTime2.Date &&
-                   dateTime1.Hour == dateTime2.Hour &&
-                   dateTime1.Minute == dateTime2.Minute &&
-                   dateTime1.Second == dateTime2.Second;
-        }
+        _examinationRepository = new ExaminationRepository();
     }
+
+    public DateTime GetEarliestFreeTimeslot(Doctor doctor)
+    {
+        var doctorExaminations = _examinationRepository.GetAll(doctor);
+        var upcomingDoctorExaminations =
+            doctorExaminations.Where(examination => examination.Start > DateTime.Now).ToList();
+
+        var earliestTimeslot = GetClosestTimeslot(DateTime.Now);
+        while (upcomingDoctorExaminations.Any(examination => AreDatesEqual(examination.Start, earliestTimeslot)))
+            earliestTimeslot = earliestTimeslot.AddMinutes(15);
+
+        return earliestTimeslot;
+    }
+
+    private DateTime GetClosestTimeslot(DateTime time)
+    {
+        var minuteStartTime = time.AddSeconds(-time.Second);
+        var validExaminationTimeslot = minuteStartTime.AddMinutes(15 - minuteStartTime.Minute % 15);
+
+        return validExaminationTimeslot;
+    }
+
+    private bool AreDatesEqual(DateTime dateTime1, DateTime dateTime2)
+    {
+        return dateTime1.Date == dateTime2.Date &&
+               dateTime1.Hour == dateTime2.Hour &&
+               dateTime1.Minute == dateTime2.Minute &&
+               dateTime1.Second == dateTime2.Second;
+    }
+
+    public bool IsIn2Hours(DateTime timeslot)
+    {
+        var closestTimeslot = GetClosestTimeslot(DateTime.Now);
+        var in2Hours = closestTimeslot.AddHours(2);
+
+        return timeslot < in2Hours || AreDatesEqual(timeslot, in2Hours);
+    }
+
+    public SortedDictionary<DateTime, Doctor> GetEarliestFreeTimeslotDoctors(List<Doctor> doctors)
+    {
+        SortedDictionary<DateTime, Doctor> result = new();
+
+        doctors.ForEach(doctor => result.Add(GetEarliestFreeTimeslot(doctor), doctor));
+
+        return result;
+    }
+
+    //public DateTime? GetEarliestFreeTimeslot()
+    //{
+
+    //}
 }
