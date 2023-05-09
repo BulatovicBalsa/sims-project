f40b612d7f5253f2e8684d4973e0bf51436ce460
Vladimir Popov <vladapopov02@gmail.com>
2023-05-09 03:45:49 +0200

    refactor: Cleanup PostponeExaminationViewModel

--- Hospital/ViewModels/Nurse/UrgentExaminations/PostponeExaminationViewModel.cs
+++ Hospital/ViewModels/Nurse/UrgentExaminations/PostponeExaminationViewModel.cs
@@ -1,80 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Net.Mime;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using Hospital.Models.Doctor;
 using Hospital.Models.Examination;
 using Hospital.Repositories.Examinaton;
 
-namespace Hospital.ViewModels.Nurse.UrgentExaminations
+namespace Hospital.ViewModels.Nurse.UrgentExaminations;
+
+public class PostponeExaminationViewModel : ViewModelBase
 {
-    public class PostponeExaminationViewModel : ViewModelBase
+    private readonly Dictionary<Doctor, DateTime> _doctorEarliestFreeTimeslot;
+    private readonly ExaminationRepository _examinationRepository;
+    private Examination? _selectedExamination;
+
+    public PostponeExaminationViewModel()
     {
-        private readonly ExaminationRepository _examinationRepository;
-        private Examination? _selectedExamination;
-        private readonly Dictionary<Doctor, DateTime> _doctorEarliestFree;
+        // dummy constructor
+    }
 
-        public PostponeExaminationViewModel()
+    public PostponeExaminationViewModel(List<Examination> examinations,
+        SortedDictionary<DateTime, Doctor> earliestFreeTimeslotDoctor)
+    {
+        Examinations = examinations;
+        _selectedExamination = null;
+        _doctorEarliestFreeTimeslot = earliestFreeTimeslotDoctor.ToDictionary(pair => pair.Value, pair => pair.Key);
+        _examinationRepository = new ExaminationRepository();
+
+        PostponeExaminationCommand =
+            new ViewModelCommand(ExecutePostponeExaminationCommand, CanExecutePostponeExaminationCommand);
+        CloseDialogCommand = new ViewModelCommand(ExecuteCloseDialogCommand);
+    }
+
+    public List<Examination> Examinations { get; }
+
+    public Examination? SelectedExamination
+    {
+        get => _selectedExamination;
+        set
         {
-            // dummy constructor
+            _selectedExamination = value;
+            OnPropertyChanged(nameof(SelectedExamination));
         }
+    }
 
-        public PostponeExaminationViewModel(List<Examination> examinations, List<KeyValuePair<Doctor, DateTime>> doctorEarliestFree)
-        {
-            Examinations = examinations;
-            _selectedExamination = null;
-            _doctorEarliestFree = doctorEarliestFree.ToDictionary(pair => pair.Key, pair => pair.Value);
-            _examinationRepository = new ExaminationRepository();
+    public ICommand PostponeExaminationCommand { get; }
+    public ICommand CloseDialogCommand { get; }
 
-            PostponeExaminationCommand =
-                new ViewModelCommand(ExecutePostponeExaminationCommand, CanExecutePostponeExaminationCommand);
-            CloseDialogCommand = new ViewModelCommand(ExecuteCloseDialogCommand);
-        }
+    public event Action<bool, DateTime?, Doctor?>? DialogClosed;
 
-        public List<Examination> Examinations { get; }
-        public Examination? SelectedExamination
-        {
-            get => _selectedExamination;
-            set
-            {
-                _selectedExamination = value;
-                OnPropertyChanged(nameof(SelectedExamination));
-            }
-        }
+    private void ExecutePostponeExaminationCommand(object obj)
+    {
+        var previousStart = SelectedExamination.Start;
+        SelectedExamination.Start = _doctorEarliestFreeTimeslot[SelectedExamination.Doctor];
+        _examinationRepository.Update(SelectedExamination, false);
 
-        public event Action<bool, DateTime?, Doctor?>? DialogClosed;
+        CloseDialog(false, previousStart, SelectedExamination.Doctor);
+    }
 
-        public ICommand PostponeExaminationCommand { get; }
-        public ICommand CloseDialogCommand { get; }
+    private bool CanExecutePostponeExaminationCommand(object obj)
+    {
+        return SelectedExamination != null;
+    }
 
-        private void ExecutePostponeExaminationCommand(object obj)
-        {
-            var previousStart = SelectedExamination.Start;
-            SelectedExamination.Start = _doctorEarliestFree[SelectedExamination.Doctor];
-            _examinationRepository.Update(SelectedExamination, false);
+    private void ExecuteCloseDialogCommand(object obj)
+    {
+        CloseDialog(true, null, null);
+    }
 
-            CloseDialog(false, previousStart, SelectedExamination.Doctor);
-        }
-
-        private bool CanExecutePostponeExaminationCommand(object obj)
-        {
-            return SelectedExamination != null;
-        }
-
-        private void ExecuteCloseDialogCommand(object obj)
-        {
-            CloseDialog(true, null, null);
-        }
-
-        private void CloseDialog(bool cancelled, DateTime? newTimeslot, Doctor? freeDoctor)
-        {
-            Application.Current.Windows[1]?.Close();
-            DialogClosed?.Invoke(cancelled, newTimeslot, freeDoctor);
-        }
+    private void CloseDialog(bool cancelled, DateTime? newTimeslot, Doctor? freeDoctor)
+    {
+        Application.Current.Windows[1]?.Close();
+        DialogClosed?.Invoke(cancelled, newTimeslot, freeDoctor);
     }
 }
