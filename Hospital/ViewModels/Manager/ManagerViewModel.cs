d56556415307c1456d968ce5a5f04217873cbc5f
MomirMilutinovic <milutinm462@gmail.com>
2023-05-11 12:54:08 -0700

    refactor: Remove unused property in ManagerViewModel

--- Hospital/ViewModels/Manager/ManagerViewModel.cs
+++ Hospital/ViewModels/Manager/ManagerViewModel.cs
@@ -9,25 +9,10 @@ public class ManagerViewModel : ViewModelBase
     public ManagerViewModel()
     {
         Children = new ObservableCollection<object>();
-        //Children.Add(new EquipmentTabViewModel());
         Children.Add(new OrderTabViewModel());
         Children.Add(new TransferTabViewModel());
-
-        OrderTabViewModel = new OrderTabViewModel();
     }
 
-    public OrderTabViewModel OrderTabViewModel
-    {
-        get => _orderTabViewModel;
-        set
-        {
-            if (_orderTabViewModel != value)
-            {
-                _orderTabViewModel = value;
-                OnPropertyChanged(nameof(OrderTabViewModel));
-            }
-        }
-    }
 
     public ObservableCollection<object> Children { get; }
 }
