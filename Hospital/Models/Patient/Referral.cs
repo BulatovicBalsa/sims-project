e242d1225a32e580c6339124566deb7a66bdae3e
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-12 22:56:24 +0200

    feat: Add Referral model

---
+++ Hospital/Models/Patient/Referral.cs
@@ -0,0 +1,23 @@
+﻿namespace Hospital.Models.Patient;
+using Doctor;
+public class Referral
+{
+    public string Specialization { get; set; }
+    public Doctor? Doctor { get; set; }
+
+    public Referral()
+    {
+        Specialization = "";
+    }
+
+    public Referral(Doctor doctor)
+    {
+        Doctor = doctor;
+        Specialization = doctor.Specialization;
+    }
+
+    public Referral(string specialization)
+    {
+        Specialization = specialization;
+    }
+}
