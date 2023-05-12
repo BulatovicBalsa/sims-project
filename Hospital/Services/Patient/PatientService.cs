e86fd1717410bb60c4d26a2ba8ba9e963b276738
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-12 22:13:17 +0200

    refactor: Move patient-related methods to Patient Service

--- Hospital/Services/Patient/PatientService.cs
+++ Hospital/Services/Patient/PatientService.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿
+using Hospital.Models.Examination;
+using Hospital.Repositories.Patient;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Hospital.Models.Patient;
 
-namespace Hospital.Services.Patient
+namespace Hospital.Services
 {
-    internal class PatientService
+    public class PatientService
     {
+        private readonly PatientRepository _patientRepository;
+
+        public PatientService()
+        {
+            _patientRepository = new();
+        }
+        
+        public Patient GetPatient(Examination examination)
+        {
+            return _patientRepository.GetById(examination.Patient!.Id)!;
+        }
+
+        public List<Patient> GetAllPatients()
+        {
+            return _patientRepository.GetAll();
+        }
+
+        public void UpdatePatient(Patient patient)
+        {
+            _patientRepository.Update(patient);
+        }
+
+        public Patient? GetPatientById(string patientId)
+        {
+            return _patientRepository.GetById(patientId);
+        }
     }
 }
