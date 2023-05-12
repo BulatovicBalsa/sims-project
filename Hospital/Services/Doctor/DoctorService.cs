fcda22cb62cb7deb60f3012b4387f227e2f4b473
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-12 22:06:36 +0200

    refactor: Move patient-related methods to Patient Service

--- Hospital/Services/Doctor/DoctorService.cs
+++ Hospital/Services/Doctor/DoctorService.cs
@@ -15,32 +15,13 @@ namespace Hospital.Coordinators;
 
 public class DoctorService
 {
-    private readonly ExaminationRepository _examinationRepository;
-    private readonly PatientRepository _patientRepository;
     private readonly RoomRepository _roomRepository;
     private readonly DoctorRepository _doctorRepository;
 
     public DoctorService()
     {
-        _examinationRepository = new ExaminationRepository();
-        _patientRepository = new PatientRepository();
         _doctorRepository = DoctorRepository.Instance;
         _roomRepository = RoomRepository.Instance;
-    }
-
-    public Patient GetPatient(Examination examination)
-    {
-        return _patientRepository.GetById(examination.Patient!.Id)!;
-    }
-
-    public List<Patient> GetAllPatients()
-    {
-        return _patientRepository.GetAll();
-    }
-
-    public void UpdatePatient(Patient patient)
-    {
-        _patientRepository.Update(patient);
     }
 
     public List<Room> GetRoomsForExamination()
@@ -48,11 +29,6 @@ public class DoctorService
         var allRooms = _roomRepository.GetAll();
         return allRooms.Where(room =>
             room.Type == RoomType.OperatingRoom || room.Type == RoomType.ExaminationRoom).ToList();
-    }
-
-    public Patient? GetPatientById(string patientId)
-    {
-        return _patientRepository.GetById(patientId);
     }
 
     public List<string> GetAllSpecializations()
