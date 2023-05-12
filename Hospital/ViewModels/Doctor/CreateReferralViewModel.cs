c5dd0fb135336963d8c05beafaea791d8cde6568
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-12 23:53:33 +0200

    feat: Implement Create Referral view model

--- Hospital/ViewModels/Doctor/CreateReferralViewModel.cs
+++ Hospital/ViewModels/Doctor/CreateReferralViewModel.cs
@@ -1,12 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using Hospital.Coordinators;
+using Hospital.Models.Doctor;
+using Hospital.Models.Patient;
 
-namespace Hospital.ViewModels.Doctor
+namespace Hospital.ViewModels
 {
-    internal class CreateReferralViewModel
+    public class CreateReferralViewModel: ViewModelBase
     {
+        private readonly DoctorService _doctorService = new();
+
+        private Doctor? _selectedDoctor;
+
+        public Doctor? SelectedDoctor
+        {
+            get => _selectedDoctor;
+            set { _selectedDoctor = value; OnPropertyChanged(nameof(SelectedDoctor)); }
+        }
+        
+        private string? _selectedSpecialization;
+
+        public string? SelectedSpecialization
+        {
+            get => _selectedSpecialization;
+            set { _selectedSpecialization = value; OnPropertyChanged(nameof(SelectedSpecialization)); }
+        }
+        private ObservableCollection<Doctor> _doctors;
+
+        public ObservableCollection<Doctor> Doctors
+        {
+            get => _doctors;
+            set { _doctors = value; OnPropertyChanged(nameof(Doctors));}
+        }
+
+        private ObservableCollection<string> _specialization;
+
+        public ObservableCollection<string> Specialization
+        {
+            get => _specialization;
+            set { _specialization = value; OnPropertyChanged(nameof(Specialization)); }
+        }
+
+        public Referral? ReferralToCreate { get; set; }
+
+        public CreateReferralViewModel(Referral? referralToCreate)
+        {
+            ReferralToCreate = referralToCreate;
+            Doctors = new ObservableCollection<Doctor>(_doctorService.GetAll());
+            Specialization = new ObservableCollection<string>(_doctorService.GetAllSpecializations());
+            CreateReferralCommand = new RelayCommand<Window>(CreateReferral);
+        }
+
+        private void CreateReferral(Window window)
+        {
+            if (SelectedDoctor is null && SelectedSpecialization is null)
+            {
+                MessageBox.Show("You must enter Specialization or Doctor");
+                return;
+            }
+
+            var referralDto = SelectedDoctor is null
+                ? new Referral(SelectedSpecialization!)
+                : new Referral(SelectedDoctor!);
+
+            ReferralToCreate?.DeepCopy(referralDto);
+
+            MessageBox.Show("Succeed");
+            window.DialogResult = true;
+        }
+
+        public ICommand CreateReferralCommand { get; set; }
+
     }
 }
