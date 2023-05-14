7498c0dd22736c58173edcf40b53524e346207fa
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-14 20:08:10 +0200

    feat: Add Prescription model

---
+++ Hospital/Models/Patient/Prescription.cs
@@ -0,0 +1,27 @@
+﻿namespace Hospital.Models.Patient;
+
+public enum MedicationTiming
+{
+    DuringMeal,
+    AfterMeal,
+    BeforeMeal,
+    Anytime
+}
+
+public class Prescription
+{
+    public Prescription()
+    {
+    }
+
+    public Prescription(int amount, int dailyUsage, MedicationTiming medicationTiming)
+    {
+        Amount = amount;
+        DailyUsage = dailyUsage;
+        MedicationTiming = medicationTiming;
+    }
+
+    public int Amount { get; set; }
+    public int DailyUsage { get; set; }
+    public MedicationTiming MedicationTiming { get; set; }
+}
