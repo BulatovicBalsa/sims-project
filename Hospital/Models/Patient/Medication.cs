c7c47409a4efc8adc21eca5e1184b44173e531cc
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-14 20:28:59 +0200

    feat: Add Medication model

---
+++ Hospital/Models/Patient/Medication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace Hospital.Models.Patient
+{
+    public class Medication
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Allergens { get; set; }
+
+        public Medication()
+        {
+            Id = Guid.NewGuid().ToString();
+            Allergens = new List<string>();
+            Name = "";
+        }
+
+        public Medication(string id, string name, List<string> allergens)
+        {
+            Id = id;
+            Name = name;
+            Allergens = allergens;
+        }
+
+        public Medication DeepCopy()
+        {
+            return new Medication(Id, Name, Allergens);
+        }
+    }
+}
