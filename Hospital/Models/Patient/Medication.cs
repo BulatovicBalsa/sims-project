1bedac826642e6f6b343b47c92211717353333ff
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-14 20:30:44 +0200

    refactor: Refactor Medication model

--- Hospital/Models/Patient/Medication.cs
+++ Hospital/Models/Patient/Medication.cs
@@ -2,31 +2,30 @@
 using System.Collections.Generic;
 using System.Windows.Documents;
 
-namespace Hospital.Models.Patient
+namespace Hospital.Models.Patient;
+
+public class Medication
 {
-    public class Medication
+    public Medication()
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public List<string> Allergens { get; set; }
+        Id = Guid.NewGuid().ToString();
+        Allergens = new List<string>();
+        Name = "";
+    }
 
-        public Medication()
-        {
-            Id = Guid.NewGuid().ToString();
-            Allergens = new List<string>();
-            Name = "";
-        }
+    public Medication(string id, string name, List<string> allergens)
+    {
+        Id = id;
+        Name = name;
+        Allergens = allergens;
+    }
 
-        public Medication(string id, string name, List<string> allergens)
-        {
-            Id = id;
-            Name = name;
-            Allergens = allergens;
-        }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public List<string> Allergens { get; set; }
 
-        public Medication DeepCopy()
-        {
-            return new Medication(Id, Name, Allergens);
-        }
+    public Medication DeepCopy()
+    {
+        return new Medication(Id, Name, Allergens);
     }
 }
