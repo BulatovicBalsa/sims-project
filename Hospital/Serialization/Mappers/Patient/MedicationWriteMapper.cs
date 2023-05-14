4cb28fbd7243a4e1e7867159f1270354074afa9d
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-14 21:40:57 +0200

    feat: Add Medication Read and Write mappers

---
+++ Hospital/Serialization/Mappers/Patient/MedicationWriteMapper.cs
@@ -0,0 +1,20 @@
+﻿using Hospital.Models.Patient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+
+namespace Hospital.Serialization.Mappers.Patient
+{
+    public sealed class MedicationWriteMapper : ClassMap<Medication>
+    {
+        public MedicationWriteMapper()
+        {
+            Map(medication => medication.Id).Index(0);
+            Map(medication => medication.Allergens)
+                .Convert(row => string.Join("|", row.Value.Allergens)).Index(1);
+        }
+    }
+}
