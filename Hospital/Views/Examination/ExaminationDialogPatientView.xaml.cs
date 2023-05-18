0ff8af7b4ae66a46fd411a258e8df5141647fa79
Teodor Vidakovic <teodorv22@gmail.com>
2023-05-18 02:03:23 +0200

    fix: removed unused parameter in examination dialog view

--- Hospital/Views/Examination/ExaminationDialogPatientView.xaml.cs
+++ Hospital/Views/Examination/ExaminationDialogPatientView.xaml.cs
@@ -16,7 +16,7 @@ namespace Hospital.Views
         private readonly ExaminationDialogViewModel _viewModel;
 
         public ExaminationDialogView(Patient patient,PatientViewModel patientViewModel, bool isUpdate, 
-             Examination examination = null, IEnumerable<Doctor> recommendedDoctors = null)
+             Examination examination = null)
         {
             InitializeComponent();
 
