994f9a6af3c13cef55c1d6aba08d21ed268f7ed0
MomirMilutinovic <milutinm462@gmail.com>
2023-05-23 03:38:08 -0700

    feat: Display all renovations in a tab

---
+++ Hospital/ViewModels/Manager/RenovationTabViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Models.Manager;
+using Hospital.Repositories.Manager;
+using Hospital.Services.Manager;
+
+namespace Hospital.ViewModels.Manager
+{
+    public class RenovationTabViewModel: ViewModelBase
+    {
+        private BindingList<Renovation> _renovations;
+
+        public BindingList<Renovation> Renovations
+        {
+            get => _renovations;
+            set
+            {
+                if (Equals(value, _renovations)) return;
+                _renovations = value;
+                OnPropertyChanged(nameof(Renovations));
+            }
+        }
+
+        public RenovationTabViewModel()
+        {
+
+            _renovations = new BindingList<Renovation>(RenovationRepository.Instance.GetAll());
+        }
+    }
+}
