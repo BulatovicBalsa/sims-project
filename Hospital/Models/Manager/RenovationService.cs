1702b20d989db6423d895472e832919415d66fe4
MomirMilutinovic <milutinm462@gmail.com>
2023-05-23 01:00:27 -0700

    feat: Add renovation from service The service checks if the renovation can be scheduled

---
+++ Hospital/Models/Manager/RenovationService.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using CsvHelper;
+using Hospital.Repositories.Manager;
+using Hospital.Scheduling;
+using Hospital.Services.Manager;
+
+namespace Hospital.Models.Manager;
+
+public class RenovationService
+{
+    private RoomScheduleService _roomScheduleService;
+    private RenovationRepository? _renovationRepository;
+
+    public RenovationService()
+    {
+        _roomScheduleService = new RoomScheduleService();
+        _renovationRepository = RenovationRepository.Instance;
+    }
+
+    public RenovationService(RoomScheduleService roomScheduleService)
+    {
+        _roomScheduleService = roomScheduleService;
+    }
+
+    public RenovationService(RoomScheduleService roomScheduleService, RenovationRepository? renovationRepository)
+    {
+        _roomScheduleService = roomScheduleService;
+        _renovationRepository = renovationRepository;
+    }
+
+    public bool AddRenovation(Renovation renovation)
+    {
+        if (!_roomScheduleService.IsFree(renovation.Room, new TimeRange(renovation.BeginTime, renovation.EndTime)))
+            return false;
+
+        _renovationRepository?.Add(renovation);
+        return true;
+    }
+
+    public void TryCompleteAllRenovations()
+    {
+        foreach (var renovation in RenovationRepository.Instance.GetAll().Where(renovation => renovation.TryComplete()))
+        {
+            if(renovation.TryComplete())
+                continue;
+            ; //TODO: Write changes
+        }
+    }
+}
---
+++ HospitalTests/Models/Manager/RenovationServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using Hospital.Models.Examination;
+using Hospital.Models.Manager;
+using Hospital.Models.Patient;
+using Hospital.Services.Manager;
+
+namespace HospitalTests.Services.Manager;
+
+[TestClass]
+public class RenovationServiceTests
+{
+    [TestMethod]
+    public void TestAddRenovationRoomHasExamination()
+    {
+        var room = new Room();
+        var examination = new Examination(null, new Patient(), true, DateTime.Now, room);
+        var examinations = new List<Examination>
+        {
+            examination
+        };
+        var renovations = new List<Renovation>
+        {
+            new("", DateTime.Now.AddDays(-5), DateTime.Now.AddDays(-3), room)
+        };
+        var roomScheduleService = new RoomScheduleService(examinations, renovations);
+        var renovationService = new RenovationService(roomScheduleService, null);
+        Assert.IsFalse(renovationService.AddRenovation(new Renovation(DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1), room)));
+    }
+
+    [TestMethod]
+    public void TestAddRenovationRoomHasRenovation()
+    {
+        var room = new Room();
+        var examination = new Examination(null, new Patient(), true, DateTime.Now, room);
+        var examinations = new List<Examination>
+        {
+            examination
+        };
+        var renovations = new List<Renovation>
+        {
+            new("", DateTime.Now.AddDays(-5), DateTime.Now.AddDays(-3), room)
+        };
+        var roomScheduleService = new RoomScheduleService(examinations, renovations);
+        var renovationService = new RenovationService(roomScheduleService, null);
+        Assert.IsFalse(renovationService.AddRenovation(new Renovation(DateTime.Now.AddDays(-4), DateTime.Now.AddDays(-3), room)));
+    }
+
+    [TestMethod]
+    public void TestAddRenovationSuccessful()
+    {
+        var roomScheduleService = new RoomScheduleService(new List<Examination>(), new List<Renovation>());
+        var renovationService = new RenovationService(roomScheduleService);
+        Assert.IsTrue(renovationService.AddRenovation(new Renovation("1", DateTime.Now, DateTime.Now.AddDays(1),
+            new Room("1", "Ward", RoomType.Ward))));
+    }
+}
