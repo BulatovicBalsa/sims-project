9573e736e67611d75810997eacc3e6bd7e8b7279
MomirMilutinovic <milutinm462@gmail.com>
2023-05-23 03:06:43 -0700

    feat: Automatically complete renovations once their completion time has passed

--- Hospital/Services/Manager/RenovationService.cs
+++ Hospital/Services/Manager/RenovationService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 using Hospital.Models.Manager;
 using Hospital.Repositories.Manager;
 using Hospital.Scheduling;
@@ -11,10 +12,16 @@ public class RenovationService
     private readonly RenovationRepository? _renovationRepository;
     private readonly RoomScheduleService _roomScheduleService;
 
+    private readonly Timer _timer;
+
     public RenovationService()
     {
         _roomScheduleService = new RoomScheduleService();
         _renovationRepository = RenovationRepository.Instance;
+        _timer = new Timer(1000);
+        _timer.Enabled = true;
+        _timer.AutoReset = true;
+        _timer.Elapsed += (sender, args) => TryCompleteAllRenovations();
     }
 
     public RenovationService(RoomScheduleService roomScheduleService)
@@ -39,10 +46,9 @@ public class RenovationService
 
     public void TryCompleteAllRenovations()
     {
-        foreach (var renovation in _renovationRepository?.GetAll().Where(renovation => renovation.TryComplete()) ?? new List<Renovation>())
-        {
+        foreach (var renovation in _renovationRepository?.GetAll().Where(renovation => renovation.TryComplete()) ??
+                                   new List<Renovation>())
             if (renovation.TryComplete())
                 RenovationRepository.Instance.Update(renovation);
-        }
     }
 }
