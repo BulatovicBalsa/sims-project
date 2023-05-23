75b8edd9c9dc9a000311f8f0668cf1f111db5ab2
Vladimir Popov <vladapopov02@gmail.com>
2023-05-24 00:49:20 +0200

    feat: Add PatientReferralViewModel

---
+++ Hospital/ViewModels/Nurse/Referrals/PatientReferralsViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Hospital.Models.Patient;
+using Hospital.Repositories.Patient;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Hospital.ViewModels.Nurse.Referrals;
+
+public class PatientReferralsViewModel : ViewModelBase
+{
+    private readonly PatientRepository _patientRepository;
+    private ObservableCollection<Patient> _patients;
+    private Patient? _selectedPatient;
+
+    public PatientReferralsViewModel()
+    {
+        _patientRepository = new PatientRepository();
+        _patients = new ObservableCollection<Patient>(_patientRepository.GetAll());
+        _selectedPatient = null;
+
+        UseReferralCommand = new ViewModelCommand(ExecuteUseReferralCommand, CanExecuteUseReferralCommand);
+    }
+
+    public Patient? SelectedPatient
+    {
+        get => _selectedPatient;
+        set
+        {
+            _selectedPatient = value;
+            OnPropertyChanged(nameof(SelectedPatient));
+        }
+    }
+
+    public ObservableCollection<Patient> Patients
+    {
+        get => _patients;
+        set
+        {
+            _patients = value;
+            OnPropertyChanged(nameof(Patients));
+        }
+    }
+
+    public ICommand UseReferralCommand { get; }
+
+    private void ExecuteUseReferralCommand(object obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private bool CanExecuteUseReferralCommand(object obj)
+    {
+        return SelectedPatient != null;
+    }
+
+}
+
