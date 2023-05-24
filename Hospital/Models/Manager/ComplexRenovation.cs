b95b3ea89993e67074281eddff403377a6b06a32
MomirMilutinovic <milutinm462@gmail.com>
2023-05-24 10:02:58 -0700

    feat: Add ComplexRenovationClass

---
+++ Hospital/Models/Manager/ComplexRenovation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Hospital.Scheduling;
+
+namespace Hospital.Models.Manager;
+
+public class ComplexRenovation
+{
+    private readonly TimeRange _time;
+    private readonly Room _leftoverEquipmentDestination;
+
+    public ComplexRenovation(List<Room> toDemolish, List<Room> toBuild, TimeRange time,
+        Room leftoverEquipmentDestination, List<Transfer> transfersFromOldToNewRooms)
+    {
+        ToDemolish = toDemolish;
+        ToBuild = toBuild;
+        _time = time;
+        _leftoverEquipmentDestination = leftoverEquipmentDestination;
+        TransfersFromOldToNewRooms = transfersFromOldToNewRooms;
+        Completed = false;
+    }
+
+    public List<Transfer> TransfersFromOldToNewRooms { get; }
+
+    public TimeRange Time { get; set; }
+
+    public DateTime BeginTime
+    {
+        get => _time.StartTime;
+        set => _time.EndTime = value;
+    }
+
+    public DateTime EndTime
+    {
+        get => _time.EndTime;
+        set => _time.EndTime = value;
+    }
+
+    public List<Room> ToDemolish { get; }
+
+    public List<Room> ToBuild { get; }
+
+    public bool Completed { get; private set; }
+
+    public bool TryComplete()
+    {
+        if (EndTime > DateTime.Now || Completed)
+            return false;
+
+        MoveEquipmentToNewRooms();
+
+        MoveLeftoverEquipment();
+
+        Completed = true;
+        return true;
+    }
+
+    private void MoveEquipmentToNewRooms()
+    {
+        foreach (var transfer in TransfersFromOldToNewRooms) transfer.TryDeliver();
+    }
+
+    private void MoveLeftoverEquipment()
+    {
+        foreach (var oldRoom in ToDemolish) oldRoom.SendAvailableInventory(_leftoverEquipmentDestination);
+    }
+}
