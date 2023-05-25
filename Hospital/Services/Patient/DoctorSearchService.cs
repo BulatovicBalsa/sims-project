cabe5bcb77b4b2cabde839a3b095b67e9f06ebc0
Teodor Vidakovic <teodorv22@gmail.com>
2023-05-25 18:50:02 +0200

    refactor: methods for filtering doctors moved from view model to service

--- Hospital/Services/Patient/DoctorSearchService.cs
+++ Hospital/Services/Patient/DoctorSearchService.cs
@@ -11,14 +11,29 @@ namespace Hospital.Services
     class DoctorSearchService
     {
         private DoctorRepository _doctorRepository;
+        private List<Doctor> _filteredDoctors;
 
         public DoctorSearchService()
         {
             _doctorRepository = DoctorRepository.Instance;
+            _filteredDoctors = new List<Doctor>();
         }
         public List<Doctor> GetAllDoctors()
         {
             return _doctorRepository.GetAll();
+        }
+        public void FilterDoctors(string firstName, string lastName, string specialization)
+        {
+            _filteredDoctors = _doctorRepository.GetAll()
+                .Where(doctor => 
+                    doctor.FirstName.ToLower().Contains(firstName.ToLower()) &&
+                    doctor.LastName.ToLower().Contains(lastName.ToLower()) &&
+                    doctor.Specialization.ToLower().Contains(specialization.ToLower()))
+                .ToList();
+        }
+        public List<Doctor> GetFilteredDoctors()
+        {
+            return _filteredDoctors;
         }
     }
 }
