807134bbccf37b380ea3f488fcde6d0b36ca7744
MomirMilutinovic <milutinm462@gmail.com>
2023-05-28 02:59:07 -0700

    test: Tests for ComplexRenovation

--- HospitalTests/Models/Manager/ComplexRenovationTests.cs
+++ HospitalTests/Models/Manager/ComplexRenovationTests.cs
@@ -1,47 +1,97 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Hospital.Models.Manager;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Hospital.Models.Manager;
 using Hospital.Scheduling;
 
-namespace HospitalTests.Models.Manager
+namespace HospitalTests.Models.Manager;
+
+[TestClass]
+public class ComplexRenovationTests
 {
-    [TestClass()]
-    public class ComplexRenovationTests
+    [TestMethod]
+    public void TestSchedule()
     {
-        [TestMethod()]
-        public void TestSchedule()
+        var toDemolish = new List<Room>
         {
-            List<Room> toDemolish = new List<Room>
-            {
-                new("Examination room", RoomType.ExaminationRoom),
-                new("Ward", RoomType.Ward)
-            };
-            List<Room> toBuild = new List<Room>
-            {
-                new Room("Operating room", RoomType.OperatingRoom)
-            };
-            Equipment equipment = new Equipment("Something", EquipmentType.DynamicEquipment);
-            toDemolish[0].SetAmount(equipment, 10);
-            Transfer fromOldToNew = new Transfer(toDemolish[0], toBuild[0], DateTime.Now);
-            fromOldToNew.AddItem(new TransferItem(equipment, 8));
+            new("Examination room", RoomType.ExaminationRoom),
+            new("Ward", RoomType.Ward)
+        };
+        var toBuild = new List<Room>
+        {
+            new("Operating room", RoomType.OperatingRoom)
+        };
+        var equipment = new Equipment("Something", EquipmentType.DynamicEquipment);
+        toDemolish[0].SetAmount(equipment, 10);
+        var fromOldToNew = new Transfer(toDemolish[0], toBuild[0], DateTime.Now);
+        fromOldToNew.AddItem(new TransferItem(equipment, 8));
 
-            ComplexRenovation.Schedule(toDemolish, toBuild, new TimeRange(DateTime.Now.AddDays(-1), DateTime.Now),
-                toBuild[0], new List<Transfer>{fromOldToNew});
+        var complexRenovation = ComplexRenovation.Schedule(toDemolish, toBuild,
+            new TimeRange(DateTime.Now.AddDays(-1), DateTime.Now),
+            toBuild[0], new List<Transfer> { fromOldToNew });
 
-            foreach (var room in toDemolish)
-            {
-                Assert.AreEqual(DateTime.Now, room.DemolitionDate);
-            }
+        foreach (var room in toDemolish) Assert.AreEqual(complexRenovation.EndTime, room.DemolitionDate);
 
-            foreach (var room in toBuild)
-            {
-               Assert.AreEqual(DateTime.Now, room.CreationDate); 
-            }
+        foreach (var room in toBuild) Assert.AreEqual(complexRenovation.EndTime, room.CreationDate);
+    }
+
+    [TestMethod]
+    public void TestTryComplete()
+    {
+        var toDemolish = new List<Room>
+        {
+            new("Examination room", RoomType.ExaminationRoom),
+            new("Ward", RoomType.Ward)
+        };
+        var toBuild = new List<Room>
+        {
+            new("Operating room", RoomType.OperatingRoom)
+        };
+        var equipment = new Equipment("Something", EquipmentType.DynamicEquipment);
+        var otherEquipment = new Equipment("Other", EquipmentType.DynamicEquipment);
+        toDemolish[0].SetAmount(equipment, 10);
+        toDemolish[1].SetAmount(otherEquipment, 3);
+        var fromOldToNew = new Transfer(toDemolish[0], toBuild[0], DateTime.Now);
+        fromOldToNew.AddItem(new TransferItem(equipment, 8));
+
+        var complexRenovation = ComplexRenovation.Schedule(toDemolish, toBuild,
+            new TimeRange(DateTime.Now.AddDays(-1), DateTime.Now),
+            toBuild[0], new List<Transfer> { fromOldToNew });
+
+        Assert.IsTrue(complexRenovation.TryComplete());
+
+        Assert.AreEqual(10, toBuild[0].GetAmount(equipment));
+        Assert.AreEqual(3, toBuild[0].GetAmount(otherEquipment));
+        Assert.AreEqual(0, toDemolish[0].GetAmount(equipment));
+        Assert.AreEqual(0, toDemolish[1].GetAmount(otherEquipment));
+    }
+
+    [TestMethod]
+    public void TestGetSimpleRenovations()
+    {
+        var toDemolish = new List<Room>
+        {
+            new("Examination room", RoomType.ExaminationRoom),
+            new("Ward", RoomType.Ward)
+        };
+        var toBuild = new List<Room>
+        {
+            new("Operating room", RoomType.OperatingRoom)
+        };
+        var equipment = new Equipment("Something", EquipmentType.DynamicEquipment);
+        toDemolish[0].SetAmount(equipment, 10);
+        var fromOldToNew = new Transfer(toDemolish[0], toBuild[0], DateTime.Now);
+        fromOldToNew.AddItem(new TransferItem(equipment, 8));
+
+        var complexRenovation = ComplexRenovation.Schedule(toDemolish, toBuild,
+            new TimeRange(DateTime.Now.AddDays(-1), DateTime.Now),
+            toBuild[0], new List<Transfer> { fromOldToNew });
+
+        var simpleRenovations = complexRenovation.GetSimpleRenovations();
+
+        Assert.AreEqual(3, simpleRenovations.Count);
+
+        foreach (var renovation in simpleRenovations)
+        {
+            Assert.AreEqual(complexRenovation.BeginTime, renovation.BeginTime);
+            Assert.AreEqual(complexRenovation.EndTime, renovation.EndTime);
         }
     }
 }
