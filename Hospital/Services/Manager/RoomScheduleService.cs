92d81c08d70f0f03ed9839e27823a886b800a3ef
MomirMilutinovic <milutinm462@gmail.com>
2023-05-28 04:40:02 -0700

    feat: Check if a room has any inbound or outbound transfers from a certain time

--- Hospital/Services/Manager/RoomScheduleService.cs
+++ Hospital/Services/Manager/RoomScheduleService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Hospital.Models.Examination;
 using Hospital.Models.Manager;
@@ -12,11 +13,13 @@ public class RoomScheduleService
 {
     private readonly List<Examination> _examinations;
     private readonly List<Renovation> _renovations;
+    private readonly List<Transfer> _transfers;
 
     public RoomScheduleService()
     {
         _examinations = ExaminationRepository.Instance.GetAll();
         _renovations = RenovationRepository.Instance.GetAll();
+        _transfers = TransferRepository.Instance.GetAll();
     }
 
     public RoomScheduleService(List<Examination> examinations, List<Renovation> renovations)
@@ -25,9 +28,27 @@ public class RoomScheduleService
         _renovations = renovations;
     }
 
+    public RoomScheduleService(List<Examination> examinations, List<Renovation> renovations, List<Transfer> transfers)
+    {
+        _examinations = examinations;
+        _renovations = renovations;
+        _transfers = transfers;
+    }
+
     public bool IsFree(Room room, TimeRange range)
     {
         return !HasExaminationsScheduled(room, range) && !HasRenovationsScheduled(room, range);
+    }
+
+    public bool IsFreeFrom(Room room, DateTime date)
+    {
+        var untilEndOfTime = new TimeRange(date, DateTime.MaxValue);
+        return !HasExaminationsScheduled(room, untilEndOfTime) && !HasRenovationsScheduled(room, untilEndOfTime);
+    }
+
+    public bool AreFreeFrom(List<Room> rooms, DateTime date)
+    {
+        return rooms.All(room => IsFreeFrom(room, date));
     }
 
     public bool HasRenovationsScheduled(Room room, TimeRange range)
@@ -42,5 +63,16 @@ public class RoomScheduleService
         var examinationsInRoom = _examinations
             .Where(examination => examination.Room != null && examination.Room.Equals(room)).ToList();
         return examinationsInRoom.Exists(examination => range.OverlapsWith(examination.Start, examination.End));
+    }
+
+    public bool HasTransfersScheduledFrom(Room room, DateTime date)
+    {
+        var transfersInRoom = _transfers.Where(transfer => transfer.Origin.Equals(room) || transfer.Destination.Equals(room)).ToList();
+        return transfersInRoom.Exists(transfer => transfer.DeliveryDateTime > date);
+    }
+
+    public bool HaveNoTransfersScheduledFrom(List<Room> rooms, DateTime date)
+    {
+        return rooms.All(room => !HasTransfersScheduledFrom(room, date));
     }
 }
