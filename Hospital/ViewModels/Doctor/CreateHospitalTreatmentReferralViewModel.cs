b48635b3f6d209caff767aeeab4e022353d376a1
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-28 18:51:23 +0200

    refactor: Refactor Create Hospital Treatment Referral view model

--- Hospital/ViewModels/Doctor/CreateHospitalTreatmentReferralViewModel.cs
+++ Hospital/ViewModels/Doctor/CreateHospitalTreatmentReferralViewModel.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
@@ -11,89 +7,92 @@ using Hospital.Models.Patient;
 using Hospital.Services;
 using Microsoft.VisualBasic;
 
-namespace Hospital.ViewModels
+namespace Hospital.ViewModels;
+
+public class CreateHospitalTreatmentReferralViewModel : ViewModelBase
 {
-    public class CreateHospitalTreatmentReferralViewModel : ViewModelBase
+    private readonly Patient _patientOnExamination;
+    private readonly PatientService _patientService = new();
+
+    private ObservableCollection<string> _additionalTests = new();
+    private int _duration;
+
+    private string? _selectedTest;
+
+    public CreateHospitalTreatmentReferralViewModel(Patient patientOnExamination,
+        HospitalTreatmentReferral referralToModify)
     {
-        private readonly PatientService _patientService = new();
-        private int _duration;
-        public int Duration
+        _patientOnExamination = patientOnExamination;
+        Referral = referralToModify;
+        Duration = 1;
+        AdditionalTests = new ObservableCollection<string>();
+
+        AddReferralCommand = new RelayCommand<Window>(AddReferral);
+        AddAdditionalTestCommand = new RelayCommand(AddAdditionalTest);
+        DeleteAdditionalTestCommand = new RelayCommand(DeleteAdditionalTest);
+    }
+
+    public int Duration
+    {
+        get => _duration;
+        set
         {
-            get => _duration;
-            set
-            {
-                _duration = value;
-                OnPropertyChanged(nameof(Duration));
-            }
+            _duration = value;
+            OnPropertyChanged(nameof(Duration));
+        }
+    }
+
+    public ObservableCollection<string> AdditionalTests
+    {
+        get => _additionalTests;
+        set
+        {
+            _additionalTests = value;
+            OnPropertyChanged(nameof(AdditionalTests));
+        }
+    }
+
+    public string? SelectedTest
+    {
+        get => _selectedTest;
+        set
+        {
+            _selectedTest = value;
+            OnPropertyChanged(nameof(SelectedTest));
+        }
+    }
+
+    public HospitalTreatmentReferral Referral { get; set; }
+
+    public ICommand AddReferralCommand { get; set; }
+    public ICommand AddAdditionalTestCommand { get; set; }
+    public ICommand DeleteAdditionalTestCommand { get; set; }
+
+    private void DeleteAdditionalTest()
+    {
+        if (SelectedTest is null)
+        {
+            MessageBox.Show("You must select additional test in order to delete it", "", MessageBoxButton.OK,
+                MessageBoxImage.Error);
+            return;
         }
 
-        private ObservableCollection<string> _additionalTests = new();
-        public ObservableCollection<string> AdditionalTests
-        {
-            get => _additionalTests;
-            set
-            {
-                _additionalTests = value;
-                OnPropertyChanged(nameof(AdditionalTests));
-            }
-        }
+        AdditionalTests.Remove(SelectedTest);
+    }
 
-        private string? _selectedTest;
-        public string? SelectedTest
-        {
-            get => _selectedTest;
-            set
-            {
-                _selectedTest = value;
-                OnPropertyChanged(nameof(SelectedTest));
-            }
-        }
+    private void AddAdditionalTest()
+    {
+        var testToAdd = Interaction.InputBox("Insert additional test: ", "Add additional test");
+        AdditionalTests.Add(testToAdd);
+    }
 
-        public HospitalTreatmentReferral Referral { get; set; }
-        private readonly Patient _patientOnExamination;
-
-        public ICommand AddReferralCommand { get; set; }
-        public ICommand AddAdditionalTestCommand { get; set; }
-        public ICommand DeleteAdditionalTestCommand { get; set; }
-
-        public CreateHospitalTreatmentReferralViewModel(Patient patientOnExamination, HospitalTreatmentReferral referralToModify)
-        {
-            _patientOnExamination = patientOnExamination;
-            Referral = referralToModify;
-            Duration = 1;
-            AdditionalTests = new ObservableCollection<string>();
-
-            AddReferralCommand = new RelayCommand<Window>(AddReferral);
-            AddAdditionalTestCommand = new RelayCommand(AddAdditionalTest);
-            DeleteAdditionalTestCommand = new RelayCommand(DeleteAdditionalTest);
-        }
-
-        private void DeleteAdditionalTest()
-        {
-            if (SelectedTest is null)
-            {
-                MessageBox.Show("You must select additional test in order to delete it", "", MessageBoxButton.OK,
-                    MessageBoxImage.Error);
-                return;
-            }
-
-            AdditionalTests.Remove(SelectedTest);
-        }
-
-        private void AddAdditionalTest()
-        {
-            var testToAdd = Interaction.InputBox($"Insert additional test: ", $"Add additional test");
-            AdditionalTests.Add(testToAdd);
-        }
-
-        private void AddReferral(Window window)
-        {
-            Referral.AdditionalTests = AdditionalTests.ToList();
-            Referral.Duration = Duration;
-            _patientOnExamination.HospitalTreatmentReferrals.Add(Referral);
-            _patientService.UpdatePatient(_patientOnExamination);
-            MessageBox.Show("Succeed");
-            window.DialogResult = true;
-        }
+    private void AddReferral(Window window)
+    {
+        Referral.AdditionalTests = AdditionalTests.ToList();
+        Referral.Duration = Duration;
+        _patientOnExamination.HospitalTreatmentReferrals.Add(Referral);
+        _patientService.UpdatePatient(_patientOnExamination);
+        MessageBox.Show("Succeed");
+        window.DialogResult = true;
     }
 }
