7e456ee637facaf561f2dfa1a2835024ce9a63a6
MomirMilutinovic <milutinm462@gmail.com>
2023-05-29 08:47:03 -0700

    feat: Add room tab in ManagerView

---
+++ Hospital/ViewModels/Manager/RoomTabViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using Hospital.Models.Manager;
+using Hospital.Repositories.Manager;
+
+namespace Hospital.ViewModels.Manager;
+
+public class RoomTabViewModel : ViewModelBase
+{
+    private BindingList<Room> _rooms;
+    private Room? _selectedRoom;
+    private BindingList<InventoryItem> _selectedRoomInventory;
+
+    public RoomTabViewModel()
+    {
+        _rooms = new BindingList<Room>(RoomRepository.Instance.GetAll());
+        _selectedRoomInventory = new BindingList<InventoryItem>();
+        _selectedRoom = null;
+    }
+
+    public BindingList<Room> Rooms
+    {
+        get => _rooms;
+        set
+        {
+            if (Equals(value, _rooms)) return;
+            _rooms = value;
+            OnPropertyChanged(nameof(Rooms));
+        }
+    }
+
+    public BindingList<InventoryItem> SelectedRoomInventory
+    {
+        get => _selectedRoomInventory;
+        set
+        {
+            if (Equals(value, _selectedRoomInventory)) return;
+            _selectedRoomInventory = value;
+            OnPropertyChanged(nameof(SelectedRoomInventory));
+        }
+    }
+
+    public Room? SelectedRoom
+    {
+        get => _selectedRoom;
+        set
+        {
+            if (Equals(value, _selectedRoom)) return;
+            _selectedRoom = value;
+            SelectedRoomInventory = _selectedRoom != null
+                ? new BindingList<InventoryItem>(_selectedRoom.Inventory)
+                : new BindingList<InventoryItem>();
+            OnPropertyChanged(nameof(SelectedRoom));
+        }
+    }
+}
