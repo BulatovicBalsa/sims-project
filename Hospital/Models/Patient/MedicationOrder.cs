695257fd9bff969ef2493f84836e39f793c1c8e6
Vladimir Popov <vladapopov02@gmail.com>
2023-05-30 00:34:30 +0200

    feat: Add MedicationOrder model

---
+++ Hospital/Models/Patient/MedicationOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hospital.Models.Patient;
+public class MedicationOrder
+{
+    public string Id { get; set; }
+    public string MedicationId { get; set; }
+    public int Quantity { get; set; }
+    public DateTime CreatedDate { get; set; }
+
+    public MedicationOrder()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
+}
