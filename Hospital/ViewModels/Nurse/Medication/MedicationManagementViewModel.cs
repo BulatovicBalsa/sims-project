e3af81f1db40c571c92a249cb744806077e8713a
Vladimir Popov <vladapopov02@gmail.com>
2023-05-29 23:31:08 +0200

    feat: Implement ExecuteGiveMedicationCommand

--- Hospital/ViewModels/Nurse/Medication/MedicationManagementViewModel.cs
+++ Hospital/ViewModels/Nurse/Medication/MedicationManagementViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
 using Hospital.Models.Patient;
+using Hospital.Repositories.Patient;
 using Hospital.Services;
 
 namespace Hospital.ViewModels.Nurse.Medication;
 public class MedicationManagementViewModel : ViewModelBase
 {
     private readonly PatientService _patientService;
+    private readonly MedicationService _medicationService;
+    private readonly PatientRepository _patientRepository;
     private ObservableCollection<Patient> _patients;
     private Patient? _selectedPatient;
     private ObservableCollection<Prescription>? _patientPrescriptions;
@@ -16,6 +20,8 @@ public class MedicationManagementViewModel : ViewModelBase
     public MedicationManagementViewModel()
     {
         _patientService = new PatientService();
+        _medicationService = new MedicationService();
+        _patientRepository = PatientRepository.Instance;
         _patients = new ObservableCollection<Patient>(_patientService.GetAllPatients());
         _selectedPatient = null;
         _patientPrescriptions = null;
@@ -72,11 +78,34 @@ public class MedicationManagementViewModel : ViewModelBase
 
     private void ExecuteGiveMedicationCommand(object obj)
     {
-        throw new NotImplementedException();
+        if (_medicationService.GetMedicationStock(SelectedPrescription.Medication) == 0)
+        {
+            MessageBox.Show("Selected medication is out of stock!", "Error");
+            return;
+        }
+        else if (!SelectedPrescription.CanBeDispensed())
+        {
+            MessageBox.Show("It is still too early to dispense selected medicine!", "Error");
+            return;
+        }
+
+        SelectedPrescription.LastUsed = DateTime.Now;
+        _patientRepository.Update(SelectedPatient);
+        _medicationService.DecrementMedicationStock(SelectedPrescription.Medication);
+        MessageBox.Show("Medication successfully dispensed!", "Success");
+
+        ResetInput();
     }
 
     private bool CanExecuteGiveMedicationCommand(object obj)
     {
         return SelectedPatient != null && SelectedPrescription != null;
+    }
+
+    private void ResetInput()
+    {
+        SelectedPatient = null;
+        PatientPrescriptions = null;
+        SelectedPrescription = null;
     }
 }
