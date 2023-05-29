b05fae59cc8abce8f0793a60ce3b2942ac870da4
Vladimir Popov <vladapopov02@gmail.com>
2023-05-29 23:05:48 +0200

    feat: Add MedicationService with a method to get medication stock

---
+++ Hospital/Services/Patient/MedicationService.cs
@@ -0,0 +1,20 @@
+﻿using Hospital.Models.Patient;
+using Hospital.Repositories.Patient;
+
+namespace Hospital.Services;
+public class MedicationService
+{
+    private readonly MedicationRepository _medicationRepository;
+
+    public MedicationService()
+    {
+        _medicationRepository = MedicationRepository.Instance;
+    }
+
+    public int GetMedicationStock(Medication medication)
+    {
+        var requestedMedication = _medicationRepository.GetById(medication.Id);
+
+        return requestedMedication?.Stock ?? 0;
+    }
+}
