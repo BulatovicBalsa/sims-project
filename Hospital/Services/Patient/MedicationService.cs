ac2a1392c117cbf357432bc4fe2b865cbf232af6
Vladimir Popov <vladapopov02@gmail.com>
2023-05-30 13:24:25 +0200

    feat: Add ExecuteMedicationOrders method

--- Hospital/Services/Patient/MedicationService.cs
+++ Hospital/Services/Patient/MedicationService.cs
@@ -6,10 +6,12 @@ namespace Hospital.Services;
 public class MedicationService
 {
     private readonly MedicationRepository _medicationRepository;
+    private readonly MedicationOrderRepository _medicationOrderRepository;
 
     public MedicationService()
     {
         _medicationRepository = MedicationRepository.Instance;
+        _medicationOrderRepository = MedicationOrderRepository.Instance;
     }
 
     public int GetMedicationStock(Medication medication)
@@ -23,6 +25,24 @@ public class MedicationService
     {
         medication.Stock--;
         _medicationRepository.Update(medication);
+    }
+
+    public void IncrementMedicationStock(string medicationId, int quantity)
+    {
+        var medicationToUpdate = _medicationRepository.GetById(medicationId);
+        medicationToUpdate.Stock += quantity;
+        _medicationRepository.Update(medicationToUpdate);
+    }
+
+    public void ExecuteMedicationOrders()
+    {
+        var executableOrders = _medicationOrderRepository.GetAllExecutable();
+
+        foreach (var executableOrder in executableOrders)
+        {
+            IncrementMedicationStock(executableOrder.MedicationId, executableOrder.Quantity);
+            _medicationOrderRepository.Delete(executableOrder);
+        }
     }
 
     public List<Medication> GetLowStockMedication()
