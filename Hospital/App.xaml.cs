5dc982f1361ea5f1de68f7a39ea3508d3a1da811
Vladimir Popov <vladapopov02@gmail.com>
2023-05-30 13:57:33 +0200

    feat: Add execution of medication orders on application start

--- Hospital/App.xaml.cs
+++ Hospital/App.xaml.cs
@@ -17,12 +17,13 @@ public partial class App : Application
 {
     private const string _unsuccessfulLoginMessage = "Login was not successful.";
     private readonly MedicationOrderService _medicationOrderService = new();
-    private readonly System.Timers.Timer _medicationOrderTimer = new System.Timers.Timer(60000);
+    private readonly System.Timers.Timer _medicationOrderTimer = new(60000);
 
     private void ProcessEventsThatOccurredBeforeAppStart()
     {
         EquipmentOrderService.AttemptPickUpOfAllOrders();
         TransferService.AttemptDeliveryOfAllTransfers();
+        _medicationOrderService.ExecuteMedicationOrders();
     }
 
     protected void ApplicationStart(object sender, EventArgs e)
