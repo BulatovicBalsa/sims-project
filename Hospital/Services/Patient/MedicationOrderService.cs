5d42749cf6de634a9041bce3fb8d8c580e1fb658
Vladimir Popov <vladapopov02@gmail.com>
2023-05-30 13:05:05 +0200

    feat: Add MedicationOrderService

---
+++ Hospital/Services/Patient/MedicationOrderService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Hospital.Models.Patient;
+using Hospital.Repositories.Patient;
+using Hospital.ViewModels.Nurse.Medication;
+
+namespace Hospital.Services;
+public class MedicationOrderService
+{
+    private readonly MedicationOrderRepository _medicationOrderRepository;
+
+    public MedicationOrderService()
+    {
+        _medicationOrderRepository = MedicationOrderRepository.Instance;
+    }
+
+    public void AddNewOrder(MedicationOrderQuantityDto medicationOrderQuantityDto)
+    {
+        var medicationOrder = new MedicationOrder()
+        {
+            MedicationId = medicationOrderQuantityDto.MedicationId,
+            Quantity = medicationOrderQuantityDto.OrderQuantity,
+            CreatedDate = DateTime.Now
+        };
+
+        _medicationOrderRepository.Add(medicationOrder);
+    }
+}
