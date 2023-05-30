6e4c4bcafb3cc087fe36d0a3d30178727dd6ea19
MomirMilutinovic <milutinm462@gmail.com>
2023-05-30 12:07:56 -0700

    feat: Add start and end time inputs to MergeRoom

--- Hospital/ViewModels/Manager/MergeRoomsViewModel.cs
+++ Hospital/ViewModels/Manager/MergeRoomsViewModel.cs
@@ -1,45 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
 using Hospital.Models.Manager;
+using Hospital.Scheduling;
 
-namespace Hospital.ViewModels.Manager
+namespace Hospital.ViewModels.Manager;
+
+public class MergeRoomsViewModel : ViewModelBase
 {
-    public class MergeRoomsViewModel: ViewModelBase
+    private readonly RelayCommand<IClosable> _mergeRoomsCommand;
+    private Room _newRoom;
+    private TimeRange _timeRange;
+
+    public MergeRoomsViewModel(List<Room> toMerge)
     {
-        private RelayCommand<IClosable> _mergeRoomsCommand;
-        private Room _newRoom;
-        public List<Room> ToMerge { get; set; }
+        _mergeRoomsCommand = new RelayCommand<IClosable>(MergeRooms);
+        _timeRange = new TimeRange(DateTime.Now, DateTime.Now);
+        ToMerge = toMerge;
+        NewRoom = new Room();
+        NewRoom.Type = RoomType.ExaminationRoom;
+    }
 
-        public Room NewRoom
+    public TimeRange TimeRange
+    {
+        get => _timeRange;
+        set
         {
-            get => _newRoom;
-            set
-            {
-                if (Equals(value, _newRoom)) return;
-                _newRoom = value;
-                OnPropertyChanged(nameof(NewRoom));
-            }
+            if (Equals(value, _timeRange)) return;
+            _timeRange = value;
+            OnPropertyChanged(nameof(TimeRange));
         }
+    }
 
-        public MergeRoomsViewModel(List<Room> toMerge)
+    public List<Room> ToMerge { get; set; }
+
+    public Room NewRoom
+    {
+        get => _newRoom;
+        set
         {
-            _mergeRoomsCommand = new RelayCommand<IClosable>(MergeRooms);
-            ToMerge = toMerge;
-            NewRoom = new Room();
-            NewRoom.Type = RoomType.ExaminationRoom;
-            
+            if (Equals(value, _newRoom)) return;
+            _newRoom = value;
+            OnPropertyChanged(nameof(NewRoom));
         }
+    }
 
-        public ICommand MergeRoomsCommand => _mergeRoomsCommand;
+    public ICommand MergeRoomsCommand => _mergeRoomsCommand;
 
-        private void MergeRooms(IClosable window)
-        {
-            window.Close();
-        }
+    private ComplexRenovation GetComplexRenovation()
+    {
+        return new ComplexRenovation(ToMerge, new List<Room> { NewRoom }, TimeRange, NewRoom, new List<Transfer>());
+    }
+
+    private void MergeRooms(IClosable window)
+    {
+        if (!Validate()) return;
+        window.Close();
+    }
+
+    private bool Validate()
+    {
+        if (TimeRange.StartTime <= TimeRange.EndTime) return true;
+        MessageBox.Show("Start time can not be after end time");
+        return false;
     }
 }
