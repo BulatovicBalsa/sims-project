3d71c1fd608e06d3af7ddf4666d99a4309992aac
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-31 19:15:54 +0200

    feat: Add Visit Hospital Treatment Patients view model

---
+++ Hospital/ViewModels/Doctor/VisitHospitalTreatmentPatientsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using Hospital.Models.Doctor;
+using Hospital.Models.Patient;
+using Hospital.Services;
+
+namespace Hospital.ViewModels
+{
+    public class VisitHospitalTreatmentPatientsViewModel : ViewModelBase
+    {
+        private readonly PatientService _patientService = new();
+        private Doctor _doctor;
+        private ObservableCollection<HospitalTreatmentReferral> _patientsReferrals;
+        public ObservableCollection<HospitalTreatmentReferral> PatientsReferrals
+        {
+            get => _patientsReferrals;
+            set
+            {
+                _patientsReferrals = value;
+                OnPropertyChanged(nameof(PatientsReferrals));
+            }
+        }
+
+        public VisitHospitalTreatmentPatientsViewModel(Doctor doctor)
+        {
+            _doctor = doctor;
+            _patientsReferrals = new ObservableCollection<HospitalTreatmentReferral>(_patientService.GetHospitalizedPatients(_doctor));
+        }
+    }
+}
