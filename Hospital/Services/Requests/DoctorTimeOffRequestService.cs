466907885bf5b5baf902a3401326ccaa77269fea
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-31 17:03:27 +0200

    feat: Add Doctor Time Off Request service

---
+++ Hospital/Services/Requests/DoctorTimeOffRequestService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+using Hospital.Models.Doctor;
+using Hospital.Models.Requests;
+using Hospital.Repositories.Requests;
+
+namespace Hospital.Services.Requests
+{
+    public class DoctorTimeOffRequestService
+    {
+        private readonly DoctorTimeOffRequestRepository _requestRepository = DoctorTimeOffRequestRepository.Instance;
+
+        public DoctorTimeOffRequestService() { }
+
+        public List<DoctorTimeOffRequest> GetAll()
+        {
+            return _requestRepository.GetAll();
+        }
+
+        public List<DoctorTimeOffRequest> GetNonExpiredDoctorTimeOffRequests(Doctor doctor)
+        {
+            return _requestRepository.GetNonExpiredDoctorTimeOffRequests(doctor);
+        }
+    }
+}
