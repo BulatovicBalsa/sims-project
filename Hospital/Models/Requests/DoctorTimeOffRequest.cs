5951f321d3b7bb431d1f338999f575fa9d7dde87
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-05-31 16:29:43 +0200

    refactor: Refactor Doctor Time Off Request model

--- Hospital/Models/Requests/DoctorTimeOffRequest.cs
+++ Hospital/Models/Requests/DoctorTimeOffRequest.cs
@@ -1,38 +1,37 @@
 ﻿using System;
 using Hospital.Exceptions;
 
-namespace Hospital.Models.Requests
+namespace Hospital.Models.Requests;
+
+public class DoctorTimeOffRequest
 {
-    public class DoctorTimeOffRequest
+    public DoctorTimeOffRequest()
     {
-        public string Reason { get; set; }
-        public DateTime Start { get; set; }
-        public DateTime End { get; set; }
-        public bool IsApproved { get; set; }
+        Reason = "";
+    }
 
-        public DoctorTimeOffRequest()
-        {
-            Reason = "";
-        }
+    public DoctorTimeOffRequest(string reason, DateTime start, DateTime end, bool isApproved = false)
+    {
+        if (string.IsNullOrEmpty(reason))
+            throw new UndefinedTimeOffReasonException("Reason can't be empty");
+        Reason = reason.Trim();
+        Start = start.Date;
+        End = end.Date;
+        IsApproved = isApproved;
+        CheckValidity();
+    }
 
-        public DoctorTimeOffRequest(string reason, DateTime start, DateTime end, bool isApproved=false)
-        {
-            if (string.IsNullOrEmpty(reason))
-                throw new UndefinedTimeOffReasonException("Reason can't be empty");
-            Reason = reason.Trim();
-            Start = start.Date;
-            End = end.Date;
-            IsApproved = isApproved;
-            CheckValidity();
-        }
+    public string Reason { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public bool IsApproved { get; set; }
 
-        private void CheckValidity()
-        {
-            if (string.IsNullOrEmpty(Reason))
-                throw new UndefinedTimeOffReasonException("Reason must be defined");
-            var difference = Start - DateTime.Today;
-            if (difference.TotalDays < 2)
-                throw new InvalidOperationException("The start date must be at least 2 days away from today.");
-        }
+    private void CheckValidity()
+    {
+        if (string.IsNullOrEmpty(Reason))
+            throw new UndefinedTimeOffReasonException("Reason must be defined");
+        var difference = Start - DateTime.Today;
+        if (difference.TotalDays < 2)
+            throw new InvalidOperationException("The start date must be at least 2 days away from today.");
     }
 }
