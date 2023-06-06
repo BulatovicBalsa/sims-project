7a334f6c4d690237170f0c14ac7b0640d7e81c3c
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-06-06 18:01:18 +0200

    refactor: Cleanup Hospital Treatment Service

--- Hospital/Services/HospitalTreatmentService.cs
+++ Hospital/Services/HospitalTreatmentService.cs
@@ -1,27 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Hospital.DTOs;
 using Hospital.Models.Doctor;
-using Hospital.Models.Patient;
-using Hospital.Repositories.Examination;
-using Hospital.Repositories.Patient;
 
-namespace Hospital.Services
+namespace Hospital.Services;
+
+public class HospitalTreatmentService
 {
-    public class HospitalTreatmentService
+    private readonly ExaminationService _examinationService = new();
+
+    public List<MedicalVisitDto> GetHospitalizedPatients(Doctor doctor)
     {
-        private readonly ExaminationService _examinationService = new();
+        var designatedPatients = _examinationService.GetViewedPatients(doctor);
 
-        public HospitalTreatmentService() { }
-
-        public List<MedicalVisitDto> GetHospitalizedPatients(Doctor doctor)
-        {
-            var designatedPatients = _examinationService.GetViewedPatients(doctor);
-
-            return (from patient in designatedPatients where patient.HospitalTreatmentReferrals.Count > 0 select new MedicalVisitDto(patient, patient.HospitalTreatmentReferrals[0])).ToList();
-        }
+        return (from patient in designatedPatients
+            where patient.IsHospitalized()
+            select new MedicalVisitDto(patient, patient.GetActiveHospitalTreatmentReferral())).ToList();
     }
 }
