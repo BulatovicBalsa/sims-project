811986be58eac70bfc8b75015ed50dd1fc793725
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-06-08 00:58:52 +0200

    feat: Add Menu Handler

--- HospitalCLI/CliViews/DoctorCli.cs
+++ HospitalCLI/CliViews/DoctorCli.cs
@@ -1,21 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Hospital.Models.Doctor;
+using Hospital.Models.Patient;
 using Hospital.Services;
+using Hospital.ViewModels;
 
 namespace HospitalCLI.CliViews
 {
-    public class DoctorCli
+    public class DoctorCli : DoctorViewModel
     {
-        private DoctorService _doctorService = new();
-        private Doctor? _doctor;
+        private static readonly DoctorService DoctorService = new();
 
-        public DoctorCli()
+        public DoctorCli() : base(DoctorService.GetById(Thread.CurrentPrincipal!.Identity!.Name!.Split('|')[0])!)
         {
-            _doctor = _doctorService.GetById(Thread.CurrentPrincipal!.Identity!.Name!.Split('|')[0]);
+            
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("1 - View Examinations for next 3 days");
+            Console.WriteLine("2 - View Examinations for selected date");
+            Console.WriteLine("3 - Add Examination");
+            Console.WriteLine("4 - Update Examination");
+            Console.WriteLine("5 - Delete Examination");
+            Console.WriteLine("X - Exit");
+
+            var dialogResult = Console.ReadLine();
+
+            switch (dialogResult)
+            {
+                case "1":
+                    Console.WriteLine(Examinations.ToList());
+                    break;
+                case "2":
+                    ViewExaminationsForSelectedDate();
+                    break;
+            }
+        }
+
+        private void ViewExaminationsForSelectedDate()
+        {
+            Console.WriteLine("Enter date you want to see (yyyy MM dd): ");
+            var dateAsString = Console.ReadLine();
+            if (!DateTime.TryParseExact(dateAsString, "yyyy MM dd", null, System.Globalization.DateTimeStyles.None, out var selectedDate)) return;
+            SelectedDate = selectedDate;
+            Console.WriteLine(Examinations.ToList());
         }
     }
 }
