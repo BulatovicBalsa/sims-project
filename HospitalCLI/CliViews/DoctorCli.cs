21efe3cdcd1765e2a930483ec1f8cd63ecf93f9b
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-06-08 00:41:55 +0200

    feat: Create DoctorCli constructor

--- HospitalCLI/CliViews/DoctorCli.cs
+++ HospitalCLI/CliViews/DoctorCli.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hospital.Models.Doctor;
+using Hospital.Services;
 
 namespace HospitalCLI.CliViews
 {
-    internal class DoctorCli
+    public class DoctorCli
     {
+        private DoctorService _doctorService = new();
+        private Doctor? _doctor;
+
+        public DoctorCli()
+        {
+            _doctor = _doctorService.GetById(Thread.CurrentPrincipal!.Identity!.Name!.Split('|')[0]);
+        }
     }
 }
