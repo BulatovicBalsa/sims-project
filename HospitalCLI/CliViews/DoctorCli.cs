27c11f069b46b342e5e66cbea1a7f14c40e1b7ee
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-06-08 02:14:21 +0200

    feat: Add AddExaminationCli method

--- HospitalCLI/CliViews/DoctorCli.cs
+++ HospitalCLI/CliViews/DoctorCli.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.ObjectModel;
 using Hospital.Models.Examination;
+using Hospital.Models.Manager;
+using Hospital.Models.Patient;
 using Hospital.Services;
+using Hospital.Services.Manager;
 using Hospital.ViewModels;
 
 namespace HospitalCLI.CliViews
@@ -8,9 +11,13 @@ namespace HospitalCLI.CliViews
     public class DoctorCli : DoctorViewModel
     {
         private static readonly DoctorService DoctorService = new();
+        private readonly List<Patient> _allPatients;
+        private readonly List<Room> _allRooms;
 
         public DoctorCli() : base(DoctorService.GetById(Thread.CurrentPrincipal!.Identity!.Name!.Split('|')[0])!)
         {
+            _allPatients = _patientService.GetAllPatients();
+            _allRooms = new RoomFilterService().GetRoomsForExamination();
             Menu();
         }
 
@@ -36,6 +43,7 @@ namespace HospitalCLI.CliViews
                         ViewExaminationsForSelectedDate();
                         break;
                     case "3":
+                        AddExaminationCli();
                         break;
                     case "4":
                         break;
@@ -45,6 +53,46 @@ namespace HospitalCLI.CliViews
                         Console.WriteLine("Good Bye!!!!!!");
                         return;
                 }
+            }
+        }
+
+        private void AddExaminationCli()
+        {
+            var i = 0;
+            var j = 0;
+
+            Console.Write("Date and time: (yyyy-MM-dd HH:mm)");
+            var dateAsString = Console.ReadLine();
+            if (!DateTime.TryParseExact(dateAsString, "yyyy-MM-dd HH:mm", null, System.Globalization.DateTimeStyles.None, out var selectedDate)) return;
+            
+            Console.WriteLine("Patients: ");
+            _allPatients.ForEach(patient => Console.WriteLine($"{i++}, {patient}"));
+
+            Console.Write("Enter patient index: ");
+            var index = Convert.ToInt32(Console.ReadLine());
+            var selectedPatient = _allPatients[index];
+
+            Console.WriteLine("Rooms: ");
+            _allRooms.ForEach(room => Console.WriteLine($"{j++} {room}"));
+
+            Console.Write("Enter room index: ");
+            index = Convert.ToInt32(Console.ReadLine());
+            var selectedRoom = _allRooms[index];
+
+            var isOperation = false;
+            Console.WriteLine("Is operation?: ");
+            if (Console.ReadLine() == "yes") isOperation = true;
+
+            var examination =
+                new Examination(_doctor, selectedPatient, isOperation, selectedDate, selectedRoom);
+
+            try
+            {
+                _examinationService.AddExamination(examination, false);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
 
