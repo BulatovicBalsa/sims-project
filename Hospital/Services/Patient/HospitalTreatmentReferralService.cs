44822018f6da5345d63f4f005788f9f4059081de
Vladimir Popov <vladapopov02@gmail.com>
2023-06-13 01:30:58 +0200

    feat: Add HospitalTreatmentReferralService

---
+++ Hospital/Services/Patient/HospitalTreatmentReferralService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Hospital.Models.Patient;
+using Hospital.Repositories.Patient;
+
+namespace Hospital.Services.Patient;
+public class HospitalTreatmentReferralService
+{
+    private readonly PatientRepository _patientRepository;
+
+    public HospitalTreatmentReferralService()
+    {
+        _patientRepository = PatientRepository.Instance;
+    }
+
+    public List<HospitalTreatmentReferral> GetAllActiveHospitalTreatmentReferrals()
+    {
+        var allPatients = _patientRepository.GetAll();
+        var activeHospitalTreatmentReferrals =
+            allPatients.Select(patient => patient.GetActiveHospitalTreatmentReferral());
+
+        return activeHospitalTreatmentReferrals.Where(referral => referral != null).ToList()!;
+    }
+}
