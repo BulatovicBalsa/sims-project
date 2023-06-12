ea29c99f10f97cab2c013d47893bb2784ccf8a47
Vladimir Popov <vladapopov02@gmail.com>
2023-06-13 00:45:51 +0200

    feat: Add AccommodablePatients and SelectedPatient properties tto PatientAccommodationViewModel

--- Hospital/ViewModels/Nurse/PatientAccommodation/PatientAccommodationViewModel.cs
+++ Hospital/ViewModels/Nurse/PatientAccommodation/PatientAccommodationViewModel.cs
@@ -1,5 +1,38 @@
-﻿namespace Hospital.ViewModels.Nurse.PatientAccommodation;
+﻿using System.Collections.ObjectModel;
+using Hospital.Models.Patient;
+using Hospital.Services;
+
+namespace Hospital.ViewModels.Nurse.PatientAccommodation;
 public class PatientAccommodationViewModel : ViewModelBase
 {
+    private readonly PatientService _patientService;
+    private ObservableCollection<Patient> _accommodablePatients;
+    private Patient? _selectedPatient;
 
+    public PatientAccommodationViewModel()
+    {
+        _patientService = new PatientService();
+        _accommodablePatients = new ObservableCollection<Patient>(_patientService.GetAllAccommodablePatients());
+        _selectedPatient = null;
+    }
+
+    public ObservableCollection<Patient> AccommodablePatients
+    {
+        get => _accommodablePatients;
+        set
+        {
+            _accommodablePatients = value;
+            OnPropertyChanged(nameof(AccommodablePatients));
+        }
+    }
+
+    public Patient SelectedPatient
+    {
+        get => _selectedPatient;
+        set
+        {
+            _selectedPatient = value;
+            OnPropertyChanged(nameof(SelectedPatient));
+        }
+    }
 }
