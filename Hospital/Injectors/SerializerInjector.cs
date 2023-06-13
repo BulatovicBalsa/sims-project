bf891eafd14c0ee40ac588edd1b1d486eb1c289b
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-06-13 03:37:46 +0200

    feat: remove GetDoctorRepository and add dictionary with Create Instance

--- Hospital/Injectors/SerializerInjector.cs
+++ Hospital/Injectors/SerializerInjector.cs
@@ -8,22 +8,15 @@ namespace Hospital.Injectors;
 
 public class SerializerInjector
 {
-    public static DoctorRepository GetDoctorRepository()
-    {
-        ISerializer<Doctor> serializer = new Serializer<Doctor>();
-        return new DoctorRepository(serializer);
-    }
-
     private static readonly Dictionary<Type, object> Implementations = new()
     {
         { typeof(ISerializer<Doctor>), new Serializer<Doctor>() },
-
         // Add more implementations here
     };
 
     public static T CreateInstance<T>()
     {
-        Type type = typeof(T);
+        var type = typeof(T);
 
         if (Implementations.TryGetValue(type, out var implementation))
         {
