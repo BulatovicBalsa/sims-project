d47e5598fa459ba28d18c795ecbc54d45103f790
Vladimir Popov <vladapopov02@gmail.com>
2023-06-13 21:03:36 +0200

    feat: Add remaining properties and AccomodatePatientCommand

--- Hospital/ViewModels/Nurse/PatientAccommodation/PatientAccommodationViewModel.cs
+++ Hospital/ViewModels/Nurse/PatientAccommodation/PatientAccommodationViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Hospital.Models.Manager;
 using Hospital.Models.Patient;
 using Hospital.Services;
 
@@ -6,14 +9,22 @@ namespace Hospital.ViewModels.Nurse.PatientAccommodation;
 public class PatientAccommodationViewModel : ViewModelBase
 {
     private readonly PatientService _patientService;
+    private readonly HospitalTreatmentReferralService _hospitalTreatmentReferralService;
     private ObservableCollection<Patient> _accommodablePatients;
     private Patient? _selectedPatient;
+    private ObservableCollection<Room>? _availableRooms;
+    private Room? _selectedRoom;
 
     public PatientAccommodationViewModel()
     {
         _patientService = new PatientService();
+        _hospitalTreatmentReferralService = new HospitalTreatmentReferralService();
         _accommodablePatients = new ObservableCollection<Patient>(_patientService.GetAllAccommodablePatients());
         _selectedPatient = null;
+        _availableRooms = null;
+        _selectedRoom = null;
+        AccommodatePatientCommand =
+            new ViewModelCommand(ExecuteAccommodatePatientCommand, CanExecuteAccommodatePatientCommand);
     }
 
     public ObservableCollection<Patient> AccommodablePatients
@@ -33,6 +44,43 @@ public class PatientAccommodationViewModel : ViewModelBase
         {
             _selectedPatient = value;
             OnPropertyChanged(nameof(SelectedPatient));
+
+            if (value != null)
+            {
+                AvailableRooms = new ObservableCollection<Room>(_hospitalTreatmentReferralService.GetAvailableRooms());
+            }
         }
+    }
+
+    public ObservableCollection<Room>? AvailableRooms
+    {
+        get => _availableRooms;
+        set
+        {
+            _availableRooms = value;
+            OnPropertyChanged(nameof(AvailableRooms));
+        }
+    }
+
+    public Room? SelectedRoom
+    {
+        get => _selectedRoom;
+        set
+        {
+            _selectedRoom = value;
+            OnPropertyChanged(nameof(SelectedRoom));
+        }
+    }
+
+    public ICommand AccommodatePatientCommand { get; }
+
+    public void ExecuteAccommodatePatientCommand(object obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool CanExecuteAccommodatePatientCommand(object obj)
+    {
+        return SelectedPatient != null && SelectedRoom != null;
     }
 }
