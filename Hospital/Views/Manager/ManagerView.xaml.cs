83e9fec94687b23c9e14419bed9004125f43ef10
Momir Milutinović <milutinm462@gmail.com>
2023-06-13 08:57:57 -0700

    feat: Plot frequencies of only a fixed kind of rating

--- Hospital/Views/Manager/ManagerView.xaml.cs
+++ Hospital/Views/Manager/ManagerView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
 using System.Windows;
 using Hospital.Repositories.Feedback;
 using ScottPlot;
@@ -10,8 +12,33 @@ public partial class ManagerView : Window
     public ManagerView()
     {
         InitializeComponent();
+        PlotAverageHospitalFeedback();
+        PlotHospitalFeedbackRatingFrequencies();
+    }
+
+    private void PlotHospitalFeedbackRatingFrequencies()
+    {
+        var frequencies = HospitalFeedbackRepository.Instance.GetOverallRatingFrequencies();
+        var labels = frequencies.Keys.Select(e => "Rating " + e.ToString()).ToArray();
+        var values = frequencies.Values.Select(e => (double)e).ToArray();
+
+        var pie = HospitalFeedbackFrequencyPlot.Plot.AddPie(values);
+        pie.SliceLabels = labels;
+        pie.ShowLabels = true;
+        pie.ShowValues = true;
+        HospitalFeedbackFrequencyPlot.Refresh();
+        HospitalFeedbackFrequencyPlot.Plot.AxisAuto();
+    }
+
+    private void PlotAverageHospitalFeedback()
+    {
         var averageFeedbackByArea = HospitalFeedbackRepository.Instance.GetAverageGrades();
-        double[] values = { averageFeedbackByArea.OverallRating, averageFeedbackByArea.RecommendationRating, averageFeedbackByArea.ServiceQuality, averageFeedbackByArea.PatientSatisfactionRating,  averageFeedbackByArea.CleanlinessRating};
+        double[] values =
+        {
+            averageFeedbackByArea.OverallRating, averageFeedbackByArea.RecommendationRating,
+            averageFeedbackByArea.ServiceQuality, averageFeedbackByArea.PatientSatisfactionRating,
+            averageFeedbackByArea.CleanlinessRating
+        };
         string[] labels =
             { "Overall rating", "Recommendation rating", "Service quality", "Patient satisfaction", "Cleanliness" };
         var bar = WpfPlot.Plot.AddBar(values);
@@ -19,7 +46,7 @@ public partial class ManagerView : Window
         WpfPlot.Plot.SetAxisLimitsX(xMin: 0, xMax: 5.9);
         bar.Orientation = Orientation.Horizontal;
         bar.ShowValuesAboveBars = true;
-        bar.ValueFormatter = d => Math.Round(d, 2).ToString();
+        bar.ValueFormatter = d => Math.Round(d, 2).ToString(CultureInfo.InvariantCulture);
         WpfPlot.Plot.Title("Average grade by area");
         WpfPlot.Refresh();
         WpfPlot.Plot.AxisAutoY();
