24b3b394af09f5a04d33e2e1e47c01e91ad11eec
Momir Milutinović <milutinm462@gmail.com>
2023-06-14 10:49:32 -0700

    refactor: Extract NotifyPatients method in PatientNotificationHandler

--- Hospital/Services/Requests/PatientNotificationHandler.cs
+++ Hospital/Services/Requests/PatientNotificationHandler.cs
@@ -1,31 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Hospital.Models;
+using Hospital.Models.Examination;
 using Hospital.Models.Requests;
+using Hospital.Repositories;
 using Hospital.Repositories.Doctor;
 using Hospital.Repositories.Examination;
-using Hospital.Repositories;
 using Hospital.Scheduling;
 
-namespace Hospital.Services.Requests
-{
-    public class PatientNotificationHandler: AbstractApprovalHandler
-    {
-        public override void Handle(DoctorTimeOffRequest request)
-        {
-            var doctor = DoctorRepository.Instance.GetById(request.DoctorId);
-            var examinationsToBeCancelled = ExaminationRepository.Instance.GetExaminationsInTimeRange(doctor, new TimeRange(request.Start, request.End));
-            var notificationRepository = new NotificationRepository();
-            foreach (var examination in examinationsToBeCancelled)
-            {
-                notificationRepository.Add(new Notification(examination.Patient.Id,
-                    $"Examination by {examination.Doctor.FirstName} {examination.Doctor.LastName} at {examination.Start} has been cancelled."));
-            }
+namespace Hospital.Services.Requests;
 
-            base.Handle(request);
-        }
+public class PatientNotificationHandler : AbstractApprovalHandler
+{
+    public override void Handle(DoctorTimeOffRequest request)
+    {
+        var doctor = DoctorRepository.Instance.GetById(request.DoctorId);
+        var examinationsToBeCancelled =
+            ExaminationRepository.Instance.GetExaminationsInTimeRange(doctor,
+                new TimeRange(request.Start, request.End));
+        NotifyPatients(examinationsToBeCancelled);
+
+        base.Handle(request);
+    }
+
+    private static void NotifyPatients(List<Examination> examinationsToBeCancelled)
+    {
+        var notificationRepository = new NotificationRepository();
+        foreach (var examination in examinationsToBeCancelled)
+            notificationRepository.Add(new Notification(examination.Patient.Id,
+                $"Examination by {examination.Doctor.FirstName} {examination.Doctor.LastName} at {examination.Start} has been cancelled."));
     }
 }
