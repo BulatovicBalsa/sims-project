a1d4cf8157db4099288aebd39d69af705bd3cfc4
Momir Milutinović <milutinm462@gmail.com>
2023-06-14 10:25:02 -0700

    feat: Accept and decline time off requests from GUI

--- Hospital/ViewModels/Manager/DoctorTimeOffRequestViewModel.cs
+++ Hospital/ViewModels/Manager/DoctorTimeOffRequestViewModel.cs
@@ -1,89 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
 using Hospital.Models.Requests;
 using Hospital.Repositories.Requests;
 using Hospital.Services.Requests;
 
-namespace Hospital.ViewModels.Manager
+namespace Hospital.ViewModels.Manager;
+
+public class DoctorTimeOffRequestViewModel : ViewModelBase
 {
-    public class DoctorTimeOffRequestViewModel: ViewModelBase
+    private readonly RelayCommand _acceptCommand;
+    private readonly RelayCommand _rejectCommand;
+    private readonly DoctorTimeOffRequestService _timeOffRequestService;
+    private DoctorTimeOffRequest? _selectedRequest;
+    private ObservableCollection<DoctorTimeOffRequest> _timeOffRequests;
+
+    public DoctorTimeOffRequestViewModel()
     {
-        private DoctorTimeOffRequest? _selectedRequest;
-        private ObservableCollection<DoctorTimeOffRequest> _timeOffRequests;
-        private readonly DoctorTimeOffRequestService _timeOffRequestService;
-        private readonly RelayCommand _acceptCommand;
-        private readonly RelayCommand _rejectCommand;
+        TimeOffRequests =
+            new ObservableCollection<DoctorTimeOffRequest>(DoctorTimeOffRequestRepository.Instance.GetAll());
+        _timeOffRequestService = new DoctorTimeOffRequestService();
+        _acceptCommand = new RelayCommand(AcceptSelected, CanAcceptSelected);
+        _rejectCommand = new RelayCommand(RejectSelected, CanRejectSelected);
+    }
 
-        public ObservableCollection<DoctorTimeOffRequest> TimeOffRequests
+    public ObservableCollection<DoctorTimeOffRequest> TimeOffRequests
+    {
+        get => _timeOffRequests;
+        set
         {
-            get => _timeOffRequests;
-            set
-            {
-                if (Equals(value, _timeOffRequests)) return;
-                _timeOffRequests = value;
-                OnPropertyChanged(nameof(TimeOffRequests));
-            }
+            if (Equals(value, _timeOffRequests)) return;
+            _timeOffRequests = value;
+            OnPropertyChanged(nameof(TimeOffRequests));
         }
+    }
 
-        public DoctorTimeOffRequest? SelectedRequest
+    public DoctorTimeOffRequest? SelectedRequest
+    {
+        get => _selectedRequest;
+        set
         {
-            get => _selectedRequest;
-            set
-            {
-                if (Equals(value, _selectedRequest)) return;
-                _selectedRequest = value;
-                RaiseCanExecuteChangedForAllCommands();
-                OnPropertyChanged(nameof(SelectedRequest));
-            }
-        }
-
-        public ICommand AcceptCommand => _acceptCommand;
-
-        public ICommand RejectCommand => _rejectCommand;
-
-        public DoctorTimeOffRequestViewModel()
-        {
-            TimeOffRequests = new ObservableCollection<DoctorTimeOffRequest>(DoctorTimeOffRequestRepository.Instance.GetAll());
-            _timeOffRequestService = new DoctorTimeOffRequestService();
-            _acceptCommand = new RelayCommand(AcceptSelected, CanAcceptSelected);
-            _rejectCommand = new RelayCommand(RejectSelected, CanRejectSelected);
-        }
-
-        private void RaiseCanExecuteChangedForAllCommands()
-        {
-
-            _acceptCommand.RaiseCanExecuteChanged();
-            _rejectCommand.RaiseCanExecuteChanged();
-        }
-
-        private void AcceptSelected()
-        {
-            if(SelectedRequest != null)
-               _timeOffRequestService.Accept(SelectedRequest); 
+            if (Equals(value, _selectedRequest)) return;
+            _selectedRequest = value;
             RaiseCanExecuteChangedForAllCommands();
+            OnPropertyChanged(nameof(SelectedRequest));
         }
-        private void RejectSelected()
-        {
-            if(SelectedRequest != null)
-               _timeOffRequestService.Reject(SelectedRequest); 
-            RaiseCanExecuteChangedForAllCommands();
-        }
+    }
 
-        private bool CanAcceptSelected()
-        {
-            return SelectedRequest is { IsApproved: false };
-        }
+    public ICommand AcceptCommand => _acceptCommand;
 
-        private bool CanRejectSelected()
-        {
-            return SelectedRequest != null;
-        }
+    public ICommand RejectCommand => _rejectCommand;
+
+    private void RaiseCanExecuteChangedForAllCommands()
+    {
+        _acceptCommand.RaiseCanExecuteChanged();
+        _rejectCommand.RaiseCanExecuteChanged();
+    }
+
+    private void RefreshTimeOffRequests()
+    {
+        TimeOffRequests =
+            new ObservableCollection<DoctorTimeOffRequest>(DoctorTimeOffRequestRepository.Instance.GetAll());
+        SelectedRequest = null;
+    }
+
+    private void AcceptSelected()
+    {
+        if (SelectedRequest != null)
+            _timeOffRequestService.Accept(SelectedRequest);
+        RaiseCanExecuteChangedForAllCommands();
+        RefreshTimeOffRequests();
+    }
+
+    private void RejectSelected()
+    {
+        if (SelectedRequest != null)
+            _timeOffRequestService.Reject(SelectedRequest);
+        RaiseCanExecuteChangedForAllCommands();
+        RefreshTimeOffRequests();
+    }
+
+    private bool CanAcceptSelected()
+    {
+        return SelectedRequest is { IsApproved: false };
+    }
+
+    private bool CanRejectSelected()
+    {
+        return SelectedRequest != null;
     }
 }
