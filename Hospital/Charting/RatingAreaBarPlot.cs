434f0644ea38e0b0b478ebb305cb9465c4efe618
Momir Milutinović <milutinm462@gmail.com>
2023-06-14 04:57:40 -0700

    refactor: Break down RatingAreaBarPlot.Plot into several methods

--- Hospital/Charting/RatingAreaBarPlot.cs
+++ Hospital/Charting/RatingAreaBarPlot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using ScottPlot;
+using ScottPlot.Plottable;
 
 namespace Hospital.Charting;
 
@@ -22,14 +23,34 @@ public class RatingAreaBarPlot : ICategoryPlot
         var values = averageRatingByArea.Values.ToArray();
         var labels = averageRatingByArea.Keys.ToArray();
         _wpfPlot.Plot.Clear();
+        PlotBarPlot(values, labels);
+    }
+
+    private void PlotBarPlot(double[] values, string[] labels)
+    {
         var bar = _wpfPlot.Plot.AddBar(values);
         _wpfPlot.Plot.YTicks(labels);
-        _wpfPlot.Plot.SetAxisLimitsX(0, MaxRating + XMargin);
+        Format(bar);
+        ZoomToFitScale();
+        ZoomToFitCategories();
+        _wpfPlot.Refresh();
+    }
+
+    private void Format(BarPlot bar)
+    {
         bar.Orientation = Orientation.Horizontal;
         bar.ShowValuesAboveBars = true;
         bar.ValueFormatter = d => Math.Round(d, 2).ToString(CultureInfo.InvariantCulture);
         _wpfPlot.Plot.Title("Average rating by area");
+    }
+
+    private void ZoomToFitCategories()
+    {
         _wpfPlot.Plot.AxisAutoY();
-        _wpfPlot.Refresh();
+    }
+
+    private void ZoomToFitScale()
+    {
+        _wpfPlot.Plot.SetAxisLimitsX(0, MaxRating + XMargin);
     }
 }
