6693718f834dd53748463905f8dcb45dc365a6fb
Momir Milutinović <milutinm462@gmail.com>
2023-06-14 10:21:31 -0700

    feat: Display doctor name in time off request instead of id

---
+++ Hospital/Converters/DoctorIdToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Hospital.Repositories.Doctor;
+
+namespace Hospital.Converters;
+
+public class DoctorIdToStringConverter : IValueConverter
+{
+    private readonly DoctorRepository _doctorRepository = DoctorRepository.Instance;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return _doctorRepository.GetById(value.ToString()).ToString();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
