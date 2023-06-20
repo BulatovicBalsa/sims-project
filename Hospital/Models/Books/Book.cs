96559f12da1f3a6c9e54b380ca49aaae9aac405c
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-06-20 15:57:19 +0200

    feat: Create book model

---
+++ Hospital/Models/Books/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hospital.Models.Books;
+
+public enum BindingType
+{
+    Hardcover,
+    Paperback
+}
+
+public class Book
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Isbn { get; set; }
+    public List<int> Udc { get; set; }
+    public BindingType BindingType { get; set; }
+
+    public Book()
+    {
+        Id = Guid.NewGuid().ToString();
+        Description = Isbn = Title = "";
+        Udc = new List<int>();
+    }
+
+    public Book(string title, string description, string isbn, List<int> udc, BindingType bindingType)
+    {
+        Id = Guid.NewGuid().ToString();
+        Title = title;
+        Description = description;
+        Isbn = isbn;
+        Udc = udc;
+        BindingType = bindingType;
+    }
+}
