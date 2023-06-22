9940d5c584bdb2787020d528970e8cb609c07d67
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-06-22 22:44:51 +0200

    feat: Update book search datagrid

--- Hospital/Views/Doctor/DoctorView.xaml.cs
+++ Hospital/Views/Doctor/DoctorView.xaml.cs
@@ -1,93 +1,72 @@
-﻿using GalaSoft.MvvmLight;
-using Hospital.Services;
-using Hospital.Exceptions;
-using Hospital.Models.Doctor;
-using Hospital.Models.Examination;
-using Hospital.Models.Patient;
-using Hospital.Repositories.Doctor;
-using Hospital.Repositories.Patient;
-using Hospital.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Hospital.Models.Books;
+using Hospital.Models.Doctor;
+using Hospital.ViewModels;
 
-namespace Hospital.Views
+namespace Hospital.Views;
+
+public partial class DoctorView : Window
 {
-    public partial class DoctorView : Window
-    {
-        private bool isUserInput = true;
-        private readonly string placeholder = "Search...";
-        private DoctorViewModel _viewModel; 
+    private readonly string placeholder = "Search...";
+    private readonly DoctorViewModel _viewModel;
+    private bool isUserInput = true;
 
-        public DoctorView(Doctor doctor)
+    public DoctorView(Doctor doctor)
+    {
+        isUserInput = false;
+
+        InitializeComponent();
+
+        _viewModel = new DoctorViewModel(doctor);
+        ConfigWindow();
+    }
+
+    private void ConfigWindow()
+    {
+        DataContext = _viewModel;
+        SizeToContent = SizeToContent.Height;
+    }
+
+    private void BtnClose_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (!isUserInput)
+        {
+            isUserInput = true;
+            return;
+        }
+
+        SearchBox.Foreground = Brushes.Black;
+        var searchText = SearchBox.Text.ToLower();
+
+        var filteredPatients = _viewModel.Books.Where(book =>
+            book.Title.ToLower().Contains(searchText) ||
+            book.Author.ToLower().Contains(searchText) ||
+            book.Language.ToString().ToLower().Contains(searchText)).ToList();
+
+        PatientsDataGrid.ItemsSource = filteredPatients;
+    }
+
+    private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+        if (SearchBox.Text == placeholder)
         {
             isUserInput = false;
-
-            InitializeComponent();
-
-            _viewModel = new DoctorViewModel(doctor);
-            ConfigWindow();
+            SearchBox.Text = "";
         }
+    }
 
-        private void ConfigWindow()
-        {
-            DataContext = _viewModel;
-            SizeToContent = SizeToContent.Height;
-        }
-
-        private void BtnClose_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (!isUserInput)
-            {
-                isUserInput = true;
-                return;
-            }
-            SearchBox.Foreground = Brushes.Black;
-            string searchText = SearchBox.Text.ToLower();
-
-            List<Book> filteredPatients = _viewModel.Books.Where(patient =>
-                patient.Title.ToLower().Contains(searchText) ||
-                patient.Isbn.ToLower().Contains(searchText) ||
-                patient.Author.ToLower().Contains(searchText) ||
-                patient.UdcAsString.ToLower().Contains(searchText)).ToList();
-
-            PatientsDataGrid.ItemsSource = filteredPatients;
-        }
-
-        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (SearchBox.Text == placeholder)
-            {
-                isUserInput = false;
-                SearchBox.Text = "";
-            }
-        }
-
-        private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(SearchBox.Text))
-            {
-                isUserInput = false;
-                SearchBox.Text = placeholder;
-                SearchBox.Foreground = Brushes.Gray;
-            }
-        }
+    private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(SearchBox.Text)) return;
+        isUserInput = false;
+        SearchBox.Text = placeholder;
+        SearchBox.Foreground = Brushes.Gray;
     }
 }
