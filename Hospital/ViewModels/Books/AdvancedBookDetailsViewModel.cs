cac18355010b04b2407fc719903d0854cb9b1712
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-06-22 23:04:29 +0200

    feat: Create View Advanced Book Details view model

---
+++ Hospital/ViewModels/Books/AdvancedBookDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+using Hospital.Models.Books;
+
+namespace Hospital.ViewModels.Books
+{
+    public class AdvancedBookDetailsViewModel : ViewModelBase
+    {
+        public Book Book { get; set; }
+
+        public AdvancedBookDetailsViewModel(Book book)
+        {
+            Book = book;
+        }
+
+
+    }
+}
