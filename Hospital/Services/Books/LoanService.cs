bbbcccd1845fb7d1c9c79415b9c9db79830741db
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-06-23 11:57:16 +0200

    feat: Create Update and GetCurrentLoans methods

--- Hospital/Services/Books/LoanService.cs
+++ Hospital/Services/Books/LoanService.cs
@@ -10,11 +10,36 @@ namespace Hospital.Services.Books;
 
 public class LoanService
 {
-    private readonly LoanRepository _loanRepository = new LoanRepository(SerializerInjector.CreateInstance<ISerializer<Loan>>());
     private readonly BookRepository _bookRepository = new(SerializerInjector.CreateInstance<ISerializer<Book>>());
+    private readonly LoanRepository _loanRepository = new(SerializerInjector.CreateInstance<ISerializer<Loan>>());
 
-    public List<Loan> GetAll(Doctor member) => _loanRepository.GetAll(member);
-    public void Add(Loan loan) => _loanRepository.Add(loan);
-    public List<Book> GetNotLoanedBooks() => _bookRepository.GetAll().Where(_loanRepository.IsFree).ToList();
-    public void DeleteLoan(Loan loan) => _loanRepository.Delete(loan);
+    public List<Loan> GetAll(Doctor member)
+    {
+        return _loanRepository.GetAll(member);
+    }
+
+    public void Add(Loan loan)
+    {
+        _loanRepository.Add(loan);
+    }
+
+    public List<Book> GetNotLoanedBooks()
+    {
+        return _bookRepository.GetAll().Where(_loanRepository.IsFree).ToList();
+    }
+
+    public void DeleteLoan(Loan loan)
+    {
+        _loanRepository.Delete(loan);
+    }
+
+    public void Update(Loan loan)
+    {
+        _loanRepository.Update(loan);
+    }
+
+    public List<Loan> GetCurrentLoans(Doctor doctor)
+    {
+        return _loanRepository.GetCurrentLoans(doctor);
+    }
 }
