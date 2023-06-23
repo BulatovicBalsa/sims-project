55929db16f433aeb2bcbe7fa22250e69c3bb1f5b
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-06-23 09:50:11 +0200

    feat: add GetNotLoanedBooksMethod

--- Hospital/Services/Books/LoanService.cs
+++ Hospital/Services/Books/LoanService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Hospital.Injectors;
 using Hospital.Models.Books;
 using Hospital.Models.Doctor;
@@ -10,7 +11,10 @@ namespace Hospital.Services.Books;
 public class LoanService
 {
     private readonly LoanRepository _loanRepository = new LoanRepository(SerializerInjector.CreateInstance<ISerializer<Loan>>());
+    private readonly BookRepository _bookRepository = new(SerializerInjector.CreateInstance<ISerializer<Book>>());
 
     public List<Loan> GetAll(Doctor member) => _loanRepository.GetAll(member);
     public void Add(Loan loan) => _loanRepository.Add(loan);
+    public List<Book> GetNotLoanedBooks() => _bookRepository.GetAll().Where(_loanRepository.IsFree).ToList();
+    
 }
