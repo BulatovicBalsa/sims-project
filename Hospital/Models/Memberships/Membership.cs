50a2a060ada0c2589dd7655ff38da0be3cdb8354
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-07-03 10:29:35 +0200

    feat: create Membership model

---
+++ Hospital/Models/Memberships/Membership.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hospital.Models.Memberships;
+
+public class Membership
+{
+    public Membership(string name, int bookLimit, int loanDaysLimit, double price)
+    {
+        Id = Guid.NewGuid().ToString();
+        Name = name;
+        BookLimit = bookLimit;
+        LoanDaysLimit = loanDaysLimit;
+        Price = price;
+    }
+
+    public Membership()
+    {
+        Id = Guid.NewGuid().ToString();
+        Name = "";
+    }
+
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public int BookLimit { get; set; }
+    public int LoanDaysLimit { get; set; }
+    public double Price { get; set; }
+}
