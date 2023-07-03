1bcf8d12886bde78384c53e33c989341637099c0
Momir Milutinović <milutinm462@gmail.com>
2023-07-03 15:00:36 -0700

    feat: Make most read books dialog look like the rest of the app

--- Hospital/Views/Books/MostBorrowedBooksDialog.xaml.cs
+++ Hospital/Views/Books/MostBorrowedBooksDialog.xaml.cs
@@ -23,5 +23,19 @@ namespace Hospital.Views.Books
         {
             InitializeComponent();
         }
+        private void BtnClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnMinimize_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void ControlBar_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
     }
 }
