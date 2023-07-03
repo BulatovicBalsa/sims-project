105295a57ab833d85a4b68643c48c01630de9465
Momir Milutinović <milutinm462@gmail.com>
2023-07-03 11:45:33 -0700

    test: Added test that shows what happens if there have been no loans for the selected period when fetching most borrowed books

--- HospitalTests/Repositories/Books/LoanRepositoryTests.cs
+++ HospitalTests/Repositories/Books/LoanRepositoryTests.cs
@@ -78,5 +78,39 @@ namespace Hospital.Repositories.Books.Tests
             var topBooks = loanRepository.GetBooksOrderedByBorrowCount(curDate.AddDays(-3));
             Assert.AreEqual(books[0].Id, topBooks[0].Book.Id);
         }
+
+        [TestMethod()]
+        public void GetBooksOrderedByBorrowCountTestNoLoans()
+        {
+            var bookRepository = new BookRepository(SerializerInjector.CreateInstance<ISerializer<Book>>());
+            var loanRepository = new LoanRepository(SerializerInjector.CreateInstance<ISerializer<Loan>>());
+
+            var books = new List<Book>()
+            {
+                new Book("The Great Gatsby", "", "", new List<int>{1, 2}, BindingType.Hardcover, "", BookLanguage.English),
+                new Book("The Greater Gatsby", "", "", new List<int>{1, 2}, BindingType.Hardcover, "",
+                    BookLanguage.English),
+                new Book("The Greatest Gatsby", "", "", new List<int>{1, 2}, BindingType.Hardcover, "",
+                    BookLanguage.English)
+            };
+
+
+            foreach (var book in books)
+            {
+                bookRepository.Add(book);
+            }
+
+            var curDate = DateTime.Now;
+            var member = new Models.Doctor.Doctor();
+            new DoctorRepository(SerializerInjector.CreateInstance<ISerializer<Models.Doctor.Doctor>>()).Add(member);
+            for (var i = 0; i < 10; i++)
+            {
+                loanRepository.Add(new Loan(books[i % 3], member, curDate.AddDays(i * -1), curDate));
+            }
+
+            var topBooks = loanRepository.GetBooksOrderedByBorrowCount(curDate.AddDays(1));
+            Assert.AreEqual(0, topBooks.Count);
+        }
+
     }
 }
