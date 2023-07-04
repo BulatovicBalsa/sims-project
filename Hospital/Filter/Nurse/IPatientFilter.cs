75a715d157a77033a16bf1c1b6f7b2b086c8266b
Vladimir Popov <vladapopov02@gmail.com>
2023-07-04 20:15:47 +0200

    refactor: Remove excess filters

--- Hospital/Filter/Nurse/IPatientFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using Hospital.Models.Patient;
-
-namespace Hospital.Filter.Librarian;
-public interface IPatientFilter
-{
-    List<Patient> Filter(List<Patient> patientsToFilter, object valueToCompare);
-}
